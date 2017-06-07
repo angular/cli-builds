88b611af8907f22f88aa19d9360ba95e1c281f27
Travis CI User <travis@example.org>
2017-06-07 17:47:15 +0000

    b94b901 test: attempt to reduce flakiness of rebuild e2e test

--- tasks/serve.js
+++ tasks/serve.js
@@ -51,12 +51,13 @@ exports.default = Task.extend({
         }
         let clientAddress = serverAddress;
         if (serveTaskOptions.publicHost) {
-            const clientUrl = url.parse(serveTaskOptions.publicHost);
-            // very basic sanity check
-            if (!clientUrl.host) {
-                return Promise.reject(new SilentError(`'live-reload-client' must be a full URL.`));
+            let publicHost = serveTaskOptions.publicHost;
+            if (!/^\w+:\/\//.test(publicHost)) {
+                publicHost = `${serveTaskOptions.ssl ? 'https' : 'http'}://${publicHost}`;
             }
-            clientAddress = clientUrl.href;
+            const clientUrl = url.parse(publicHost);
+            serveTaskOptions.publicHost = clientUrl.host;
+            clientAddress = url.format(clientUrl);
         }
         if (serveTaskOptions.liveReload) {
             // This allows for live reload of page when changes are made to repo.
