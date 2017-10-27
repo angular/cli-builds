f96fc3960d73917e367fc300e596a12b0ed3f7e5
Travis CI User <travis@example.org>
2017-10-27 23:37:18 +0000

    f3e8418 fix(@angular/cli): Show detailed help for generate

--- commands/generate.js
+++ commands/generate.js
@@ -12,7 +12,7 @@ const app_utils_1 = require("../utilities/app-utils");
 const path = require("path");
 const Command = require('../ember-cli/lib/models/command');
 const SilentError = require('silent-error');
-const { cyan, yellow } = chalk_1.default;
+const { cyan, grey, yellow } = chalk_1.default;
 const separatorRegEx = /[\/\\]/g;
 exports.default = Command.extend({
     name: 'generate',
@@ -156,16 +156,57 @@ exports.default = Command.extend({
             schematicName
         });
     },
-    printDetailedHelp: function () {
+    printDetailedHelp: function (_options, rawArgs) {
         const engineHost = schematics_1.getEngineHost();
         const collectionName = this.getCollectionName();
         const collection = schematics_1.getCollection(collectionName);
-        const schematicNames = engineHost.listSchematics(collection);
-        this.ui.writeLine(cyan('Available schematics:'));
-        schematicNames.forEach(schematicName => {
-            this.ui.writeLine(yellow(`    ${schematicName}`));
-        });
-        this.ui.writeLine('');
+        const schematicName = rawArgs[1];
+        if (schematicName) {
+            const SchematicGetOptionsTask = require('../tasks/schematic-get-options').default;
+            const getOptionsTask = new SchematicGetOptionsTask({
+                ui: this.ui,
+                project: this.project
+            });
+            return getOptionsTask.run({
+                schematicName,
+                collectionName
+            })
+                .then((availableOptions) => {
+                const output = [];
+                output.push(cyan(`ng generate ${schematicName} ${cyan('[name]')} ${cyan('<options...>')}`));
+                availableOptions
+                    .filter(opt => opt.name !== 'name')
+                    .forEach(opt => {
+                    let text = cyan(`    --${opt.name}`);
+                    if (opt.schematicType) {
+                        text += cyan(` (${opt.schematicType})`);
+                    }
+                    if (opt.schematicDefault) {
+                        text += cyan(` (Default: ${opt.schematicDefault})`);
+                    }
+                    if (opt.description) {
+                        text += ` ${opt.description}`;
+                    }
+                    output.push(text);
+                    if (opt.aliases && opt.aliases.length > 0) {
+                        const aliasText = opt.aliases.reduce((acc, curr) => {
+                            return acc + ` -${curr}`;
+                        }, '');
+                        output.push(grey(`      aliases: ${aliasText}`));
+                    }
+                });
+                return output.join('\n');
+            });
+        }
+        else {
+            const schematicNames = engineHost.listSchematics(collection);
+            const output = [];
+            output.push(cyan('Available schematics:'));
+            schematicNames.forEach(schematicName => {
+                output.push(yellow(`    ${schematicName}`));
+            });
+            return Promise.resolve(output.join('\n'));
+        }
     }
 });
 //# sourceMappingURL=/home/travis/build/angular/angular-cli/commands/generate.js.map
