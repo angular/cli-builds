e232b30293d34267e0e5e9ee3ba4ea96ebc132be
Travis CI User <travis@example.org>
2018-01-31 21:07:15 +0000

    7fb2c94 feat(@angular/cli): support webpack 4 hooks for scripts plugin

--- plugins/scripts-webpack-plugin.js
+++ plugins/scripts-webpack-plugin.js
@@ -16,6 +16,20 @@ function addDependencies(compilation, scripts) {
         compilation.fileDependencies.push(...scripts);
     }
 }
+function hook(compiler, action) {
+    if (compiler.hooks) {
+        // Webpack 4
+        compiler.hooks.thisCompilation.tap('scripts-webpack-plugin', (compilation) => {
+            compilation.hooks.additionalAssets.tapAsync('scripts-webpack-plugin', (callback) => action(compilation, callback));
+        });
+    }
+    else {
+        // Webpack 3
+        compiler.plugin('this-compilation', (compilation) => {
+            compilation.plugin('additional-assets', (callback) => action(compilation, callback));
+        });
+    }
+}
 class ScriptsWebpackPlugin {
     constructor(options = {}) {
         this.options = options;
@@ -60,57 +74,55 @@ class ScriptsWebpackPlugin {
         const scripts = this.options.scripts
             .filter(script => !!script)
             .map(script => path.resolve(this.options.basePath || '', script));
-        compiler.plugin('this-compilation', (compilation) => {
-            compilation.plugin('additional-assets', (callback) => {
-                if (this.shouldSkip(compilation, scripts)) {
-                    if (this._cachedOutput) {
-                        this._insertOutput(compilation, this._cachedOutput, true);
-                    }
-                    addDependencies(compilation, scripts);
-                    callback();
-                    return;
+        hook(compiler, (compilation, callback) => {
+            if (this.shouldSkip(compilation, scripts)) {
+                if (this._cachedOutput) {
+                    this._insertOutput(compilation, this._cachedOutput, true);
                 }
-                const sourceGetters = scripts.map(fullPath => {
-                    return new Promise((resolve, reject) => {
-                        compilation.inputFileSystem.readFile(fullPath, (err, data) => {
-                            if (err) {
-                                reject(err);
-                                return;
+                addDependencies(compilation, scripts);
+                callback();
+                return;
+            }
+            const sourceGetters = scripts.map(fullPath => {
+                return new Promise((resolve, reject) => {
+                    compilation.inputFileSystem.readFile(fullPath, (err, data) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                        const content = data.toString();
+                        let source;
+                        if (this.options.sourceMap) {
+                            // TODO: Look for source map file (for '.min' scripts, etc.)
+                            let adjustedPath = fullPath;
+                            if (this.options.basePath) {
+                                adjustedPath = path.relative(this.options.basePath, fullPath);
                             }
-                            const content = data.toString();
-                            let source;
-                            if (this.options.sourceMap) {
-                                // TODO: Look for source map file (for '.min' scripts, etc.)
-                                let adjustedPath = fullPath;
-                                if (this.options.basePath) {
-                                    adjustedPath = path.relative(this.options.basePath, fullPath);
-                                }
-                                source = new webpack_sources_1.OriginalSource(content, adjustedPath);
-                            }
-                            else {
-                                source = new webpack_sources_1.RawSource(content);
-                            }
-                            resolve(source);
-                        });
+                            source = new webpack_sources_1.OriginalSource(content, adjustedPath);
+                        }
+                        else {
+                            source = new webpack_sources_1.RawSource(content);
+                        }
+                        resolve(source);
                     });
                 });
-                Promise.all(sourceGetters)
-                    .then(sources => {
-                    const concatSource = new webpack_sources_1.ConcatSource();
-                    sources.forEach(source => {
-                        concatSource.add(source);
-                        concatSource.add('\n;');
-                    });
-                    const combinedSource = new webpack_sources_1.CachedSource(concatSource);
-                    const filename = loader_utils_1.interpolateName({ resourcePath: 'scripts.js' }, this.options.filename, { content: combinedSource.source() });
-                    const output = { filename, source: combinedSource };
-                    this._insertOutput(compilation, output);
-                    this._cachedOutput = output;
-                    addDependencies(compilation, scripts);
-                    callback();
-                })
-                    .catch((err) => callback(err));
             });
+            Promise.all(sourceGetters)
+                .then(sources => {
+                const concatSource = new webpack_sources_1.ConcatSource();
+                sources.forEach(source => {
+                    concatSource.add(source);
+                    concatSource.add('\n;');
+                });
+                const combinedSource = new webpack_sources_1.CachedSource(concatSource);
+                const filename = loader_utils_1.interpolateName({ resourcePath: 'scripts.js' }, this.options.filename, { content: combinedSource.source() });
+                const output = { filename, source: combinedSource };
+                this._insertOutput(compilation, output);
+                this._cachedOutput = output;
+                addDependencies(compilation, scripts);
+                callback();
+            })
+                .catch((err) => callback(err));
         });
     }
 }
