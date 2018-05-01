9c93ff9e038a5116c3a82374dc7aa1bc198c3c73
Travis CI User <travis@example.org>
2018-05-01 02:28:20 +0000

    dceda35 docs: update application environments

---
+++ commands/getset.d.ts
@@ -0,0 +1,12 @@
+import { Command, Option } from '../models/command';
+export interface Options {
+    keyword: string;
+    search?: boolean;
+}
+export default class GetSetCommand extends Command {
+    readonly name: string;
+    readonly description: string;
+    readonly arguments: string[];
+    readonly options: Option[];
+    run(_options: Options): Promise<void>;
+}
