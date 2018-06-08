ed3889a3c54862e69604ff2056982be845b3f5a5
Angular Builds <circleci@angular.io>
2018-06-08 02:10:44 +0000

    abc62ed ci: use angular-cli repo instead of devkit

--- models/command.d.ts
+++ models/command.d.ts
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 import { logging } from '@angular-devkit/core';
 export interface CommandConstructor {
     new (context: CommandContext, logger: logging.Logger): Command;
