5817b11d2259150d41f0b0b45612733382b9d2e3
Angular Builds <circleci@angular.io>
2018-07-10 22:34:56 +0000

    483cbe2 refactor: add tslint-sonarts and a lot of tslint rules

--- tasks/npm-install.d.ts
+++ tasks/npm-install.d.ts
@@ -6,5 +6,5 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { logging } from '@angular-devkit/core';
-export declare type NpmInstall = (packageName: string, logger: logging.Logger, packageManager: string, projectRoot: string, save?: boolean) => void;
+export declare type NpmInstall = (packageName: string, logger: logging.Logger, packageManager: string, projectRoot: string, save?: boolean) => Promise<void>;
 export default function (packageName: string, logger: logging.Logger, packageManager: string, projectRoot: string, save?: boolean): Promise<void>;
