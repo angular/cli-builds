8250f98307108c85f79dbf6ca501e84eb51a96a8
Angular Builds <circleci@angular.io>
2018-11-01 15:43:12 +0000

    eef66f0 fix(@angular/cli): error out when command json is invalid

--- utilities/json-schema.d.ts
+++ utilities/json-schema.d.ts
@@ -5,8 +5,13 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { json, logging } from '@angular-devkit/core';
+import { BaseException, json } from '@angular-devkit/core';
 import { CommandDescription, Option, SubCommandDescription } from '../models/interface';
-export declare function parseJsonSchemaToSubCommandDescription(name: string, jsonPath: string, registry: json.schema.SchemaRegistry, schema: json.JsonObject, logger: logging.Logger): Promise<SubCommandDescription>;
-export declare function parseJsonSchemaToCommandDescription(name: string, jsonPath: string, registry: json.schema.SchemaRegistry, schema: json.JsonObject, logger: logging.Logger): Promise<CommandDescription>;
+export declare class CommandJsonPathException extends BaseException {
+    readonly path: string;
+    readonly name: string;
+    constructor(path: string, name: string);
+}
+export declare function parseJsonSchemaToSubCommandDescription(name: string, jsonPath: string, registry: json.schema.SchemaRegistry, schema: json.JsonObject): Promise<SubCommandDescription>;
+export declare function parseJsonSchemaToCommandDescription(name: string, jsonPath: string, registry: json.schema.SchemaRegistry, schema: json.JsonObject): Promise<CommandDescription>;
 export declare function parseJsonSchemaToOptions(registry: json.schema.SchemaRegistry, schema: json.JsonObject): Promise<Option[]>;
