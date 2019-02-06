259b885dd1878289589fdc7d43022e2958d73b55
Angular Builds <circleci@angular.io>
2019-02-06 05:31:30 +0000

    86356a7 fix(@schematics/angular): set cortrect file extention in config when using preprocessors (#13552)

--- commands/add-impl.d.ts
+++ commands/add-impl.d.ts
@@ -3,7 +3,6 @@ import { SchematicCommand } from '../models/schematic-command';
 import { Schema as AddCommandSchema } from './add';
 export declare class AddCommand extends SchematicCommand<AddCommandSchema> {
     readonly allowPrivateSchematics = true;
-    readonly allowAdditionalArgs = true;
     readonly packageManager: string;
     run(options: AddCommandSchema & Arguments): Promise<number | void>;
     private isPackageInstalled;
