897aef69117e83bc0e7fb676246004aec31dd040
Angular Builds <circleci@angular.io>
2019-02-09 00:29:12 +0000

    ec94f42 fix(@angular/cli): wrap `opn` in a new Promise because `opn` is already resolve is already resolve.

--- commands/doc-impl.d.ts
+++ commands/doc-impl.d.ts
@@ -9,5 +9,5 @@ import { Command } from '../models/command';
 import { Arguments } from '../models/interface';
 import { Schema as DocCommandSchema } from './doc';
 export declare class DocCommand extends Command<DocCommandSchema> {
-    run(options: DocCommandSchema & Arguments): Promise<any>;
+    run(options: DocCommandSchema & Arguments): Promise<0 | undefined>;
 }
