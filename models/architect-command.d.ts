084470992c14df05bf16762672117cb2abf1381a
Angular Builds <circleci@angular.io>
2019-04-05 03:08:14 +0000

    f9b4bdf refactor(@angular-devkit/architect-cli): cleanup stable architect imports

--- models/architect-command.d.ts
+++ models/architect-command.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import { index2 } from '@angular-devkit/architect';
+import { Architect, Target } from '@angular-devkit/architect';
 import { WorkspaceNodeModulesArchitectHost } from '@angular-devkit/architect/node';
 import { experimental, json } from '@angular-devkit/core';
 import { BaseCommandOptions, Command } from './command';
@@ -17,7 +17,7 @@ export interface ArchitectCommandOptions extends BaseCommandOptions {
     target?: string;
 }
 export declare abstract class ArchitectCommand<T extends ArchitectCommandOptions = ArchitectCommandOptions> extends Command<ArchitectCommandOptions> {
-    protected _architect: index2.Architect;
+    protected _architect: Architect;
     protected _architectHost: WorkspaceNodeModulesArchitectHost;
     protected _workspace: experimental.workspace.Workspace;
     protected _registry: json.schema.SchemaRegistry;
@@ -25,8 +25,8 @@ export declare abstract class ArchitectCommand<T extends ArchitectCommandOptions
     target: string | undefined;
     initialize(options: ArchitectCommandOptions & Arguments): Promise<void>;
     run(options: ArchitectCommandOptions & Arguments): Promise<number>;
-    protected runBepTarget<T>(command: string, configuration: index2.Target, overrides: json.JsonObject, buildEventLog: string): Promise<number>;
-    protected runSingleTarget(target: index2.Target, targetOptions: string[], commandOptions: ArchitectCommandOptions & Arguments): Promise<number>;
+    protected runBepTarget<T>(command: string, configuration: Target, overrides: json.JsonObject, buildEventLog: string): Promise<number>;
+    protected runSingleTarget(target: Target, targetOptions: string[], commandOptions: ArchitectCommandOptions & Arguments): Promise<number>;
     protected runArchitectTarget(options: ArchitectCommandOptions & Arguments): Promise<number>;
     private getProjectNamesByTarget;
     private _makeTargetSpecifier;
