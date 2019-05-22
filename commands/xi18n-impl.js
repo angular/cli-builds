9a3d36c981462881b2ecaa520f612d4f48c93be0
Angular Builds <circleci@angular.io>
2019-05-22 08:30:23 +0000

    6037692 fix(@angular/cli): show error when using x18n command on Node.js 12.0

--- commands/xi18n-impl.js
+++ commands/xi18n-impl.js
@@ -14,6 +14,12 @@ class Xi18nCommand extends architect_command_1.ArchitectCommand {
         this.target = 'extract-i18n';
     }
     async run(options) {
+        const version = process.version.substr(1).split('.');
+        if (Number(version[0]) === 12 && Number(version[1]) === 0) {
+            this.logger.error('Due to a defect in Node.js 12.0, the command is not supported on this Node.js version. '
+                + 'Please upgrade to Node.js 12.1 or later.');
+            return 1;
+        }
         return this.runArchitectTarget(options);
     }
 }
