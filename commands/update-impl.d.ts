29a461831f4863b3845bfbb73c87e485b470c064
Angular Builds <circleci@angular.io>
2019-08-27 17:54:32 +0000

    5606a4cc0 fix(@angular/cli): handle promise rejection on analytics install prompt (#15455)

--- commands/update-impl.d.ts
+++ commands/update-impl.d.ts
@@ -1,9 +1,18 @@
-import { Arguments, Option } from '../models/interface';
-import { SchematicCommand } from '../models/schematic-command';
+import * as semver from 'semver';
+import { Command } from '../models/command';
+import { Arguments } from '../models/interface';
 import { Schema as UpdateCommandSchema } from './update';
-export declare class UpdateCommand extends SchematicCommand<UpdateCommandSchema> {
+export declare class UpdateCommand extends Command<UpdateCommandSchema> {
     readonly allowMissingWorkspace = true;
-    parseArguments(_schematicOptions: string[], _schema: Option[]): Promise<Arguments>;
-    run(options: UpdateCommandSchema & Arguments): Promise<number | void>;
+    private workflow;
+    initialize(): Promise<void>;
+    executeSchematic(collection: string, schematic: string, options?: {}): Promise<{
+        success: boolean;
+        files: Set<string>;
+    }>;
+    executeMigrations(packageName: string, collectionPath: string, range: semver.Range, commit?: boolean): Promise<boolean | undefined>;
+    run(options: UpdateCommandSchema & Arguments): Promise<1 | 0 | 2>;
     checkCleanGit(): boolean;
+    createCommit(message: string, files: string[]): void;
+    findCurrentGitSha(): string | null;
 }
