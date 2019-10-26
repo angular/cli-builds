627fc2fd621654075266a96b84d61f4b11678447
Angular Builds <circleci@angular.io>
2019-10-26 05:57:08 +0000

    994475e0e build: update @angular/animations to version 9.0.0-next.14

--- tasks/install-package.js
+++ tasks/install-package.js
@@ -47,9 +47,14 @@ function installPackage(packageName, logger, packageManager = schema_1.PackageMa
 }
 exports.installPackage = installPackage;
 function installTempPackage(packageName, logger, packageManager = schema_1.PackageManager.Npm) {
-    const tempPath = fs_1.mkdtempSync(path_1.join(fs_1.realpathSync(os_1.tmpdir()), '.ng-temp-packages-'));
+    const tempPath = fs_1.mkdtempSync(path_1.join(fs_1.realpathSync(os_1.tmpdir()), 'angular-cli-packages-'));
     // clean up temp directory on process exit
-    process.on('exit', () => rimraf.sync(tempPath));
+    process.on('exit', () => {
+        try {
+            rimraf.sync(tempPath);
+        }
+        catch (_a) { }
+    });
     // setup prefix/global modules path
     const packageManagerArgs = getPackageManagerArguments(packageManager);
     const installArgs = [
