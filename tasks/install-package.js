a74396e1d76576ded24334bda5eb721232f9740e
Angular Builds <circleci@angular.io>
2019-11-20 06:56:19 +0000

    132b34ea7 build: update sass to version 1.23.7

--- tasks/install-package.js
+++ tasks/install-package.js
@@ -50,6 +50,20 @@ function installTempPackage(packageName, logger, packageManager = schema_1.Packa
         }
         catch (_a) { }
     });
+    // NPM will warn when a `package.json` is not found in the install directory
+    // Example:
+    // npm WARN enoent ENOENT: no such file or directory, open '/tmp/.ng-temp-packages-84Qi7y/package.json'
+    // npm WARN .ng-temp-packages-84Qi7y No description
+    // npm WARN .ng-temp-packages-84Qi7y No repository field.
+    // npm WARN .ng-temp-packages-84Qi7y No license field.
+    // While we can use `npm init -y` we will end up needing to update the 'package.json' anyways
+    // because of missing fields.
+    fs_1.writeFileSync(path_1.join(tempPath, 'package.json'), JSON.stringify({
+        name: 'temp-cli-install',
+        description: 'temp-cli-install',
+        repository: 'temp-cli-install',
+        license: 'MIT',
+    }));
     // setup prefix/global modules path
     const packageManagerArgs = getPackageManagerArguments(packageManager);
     const tempNodeModules = path_1.join(tempPath, 'node_modules');
