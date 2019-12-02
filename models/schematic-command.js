5bd8a4d7c34990e1c5a4c8eb4ce4b111abb3bcb2
Angular Builds <circleci@angular.io>
2019-12-02 19:11:05 +0000

    c63b5b148 fix(@angular-devkit/build-angular): support differential loading for web workers

--- models/schematic-command.js
+++ models/schematic-command.js
@@ -382,7 +382,8 @@ class SchematicCommand extends command_1.Command {
                     loggingQueue.push(`${color_1.colors.yellow('DELETE')} ${eventPath}`);
                     break;
                 case 'rename':
-                    loggingQueue.push(`${color_1.colors.blue('RENAME')} ${eventPath} => ${event.to}`);
+                    const eventToPath = event.to.startsWith('/') ? event.to.substr(1) : event.to;
+                    loggingQueue.push(`${color_1.colors.blue('RENAME')} ${eventPath} => ${eventToPath}`);
                     break;
             }
         });
