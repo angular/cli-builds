0e30d2f4e38e1132e6aa291987190d10e81661a5
Angular Builds <circleci@angular.io>
2020-02-24 21:30:29 +0000

    b27703081 fix(@angular-devkit/build-angular): fix  autoprefixer comments support in scss

--- utilities/color.js
+++ utilities/color.js
@@ -7,10 +7,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const colors = require("ansi-colors");
-exports.colors = colors;
+const ansiColors = require("ansi-colors");
 const tty_1 = require("tty");
 // Typings do not contain the function call (added in Node.js v9.9.0)
 exports.supportsColor = process.stdout instanceof tty_1.WriteStream &&
     process.stdout.getColorDepth() > 1;
+function removeColor(text) {
+    return text.replace(new RegExp(ansiColors.ansiRegex), '');
+}
+exports.removeColor = removeColor;
+// tslint:disable-next-line: no-any
+const colors = ansiColors.create();
+exports.colors = colors;
 colors.enabled = exports.supportsColor;
