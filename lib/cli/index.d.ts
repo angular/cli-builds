f36261a15045561c088d226b7111695b8fa0c89f
Angular Builds <circleci@angular.io>
2020-09-03 14:15:28 +0000

    76f8958ab test: support prelease versions in platform server E2E

--- lib/cli/index.d.ts
+++ lib/cli/index.d.ts
@@ -1,3 +1,11 @@
+export declare class Version {
+    readonly full: string;
+    readonly major: string;
+    readonly minor: string;
+    readonly patch: string;
+    constructor(full: string);
+}
+export declare const VERSION: Version;
 export default function (options: {
     testing?: boolean;
     cliArgs: string[];
