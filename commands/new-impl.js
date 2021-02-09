c61df5d305fee8393dbdaf3eb1e33c248ae5b63b
Angular Builds <circleci@angular.io>
2021-02-09 01:24:13 +0000

    52aaa8c16 fix(@angular/cli): update NPM 7 guidance

--- commands/new-impl.js
+++ commands/new-impl.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NewCommand = void 0;
 const schematic_command_1 = require("../models/schematic-command");
-const package_manager_1 = require("../utilities/package-manager");
 class NewCommand extends schematic_command_1.SchematicCommand {
     constructor() {
         super(...arguments);
@@ -14,7 +13,6 @@ class NewCommand extends schematic_command_1.SchematicCommand {
         return super.initialize(options);
     }
     async run(options) {
-        await package_manager_1.ensureCompatibleNpm(this.context.root);
         // Register the version of the CLI in the registry.
         const packageJson = require('../package.json');
         const version = packageJson.version;
