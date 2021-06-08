4fe02058a702a33c9a77d469a50d44c0c6ed38fd
Angular Builds <circleci@angular.io>
2021-06-08 16:44:14 +0000

    82c438bed refactor(@schematics/angular): clean up web-worker schematic

--- lib/config/workspace-schema.d.ts
+++ lib/config/workspace-schema.d.ts
@@ -735,6 +735,7 @@ export interface AngularWebWorkerOptionsSchema {
     snippet?: boolean;
     /**
      * The target to apply web worker to.
+     * @deprecated No longer has an effect.
      */
     target?: string;
 }
