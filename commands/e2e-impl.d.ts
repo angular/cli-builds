bdaeca6c11f1c3a9b4fbebaa0b1d8944b6b204f4
Angular Builds <circleci@angular.io>
2021-06-24 13:24:49 +0000

    3a40601f3 build: update to new remote instance name for RBE

--- commands/e2e-impl.d.ts
+++ commands/e2e-impl.d.ts
@@ -11,6 +11,6 @@ import { Schema as E2eCommandSchema } from './e2e';
 export declare class E2eCommand extends ArchitectCommand<E2eCommandSchema> {
     readonly target = "e2e";
     readonly multiTarget = true;
-    readonly missingTargetError = "\nCannot find \"e2e\" target for the specified project.\n\nYou should add a package that implements end-to-end testing capabilities.\n\nFor example:\n  Cypress: ng add @cypress/schematic\n  Nightwatch: ng add @nightwatch/schematics\n  WebdriverIO: ng add @wdio/schematics\n\nMore options will be added to the list as they become available.\n";
+    readonly missingTargetError = "\nCannot find \"e2e\" target for the specified project.\n\nYou should add a package that implements end-to-end testing capabilities.\n\nFor example:\n  Cypress: ng add @cypress/schematic\n  WebdriverIO: ng add @wdio/schematics\n\nMore options will be added to the list as they become available.\n";
     initialize(options: E2eCommandSchema & Arguments): Promise<number | void>;
 }
