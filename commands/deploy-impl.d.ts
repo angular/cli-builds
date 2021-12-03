cb4a734f6ed9bb9cfafa7f8aff85e51539da79ba
Angular Builds <circleci@angular.io>
2021-12-03 16:44:16 +0000

    033c4415e docs: remove @zeit/ng-deploy from deploy suggestions

--- commands/deploy-impl.d.ts
+++ commands/deploy-impl.d.ts
@@ -10,6 +10,6 @@ import { Arguments } from '../models/interface';
 import { Schema as DeployCommandSchema } from './deploy';
 export declare class DeployCommand extends ArchitectCommand<DeployCommandSchema> {
     readonly target = "deploy";
-    readonly missingTargetError = "\nCannot find \"deploy\" target for the specified project.\n\nYou should add a package that implements deployment capabilities for your\nfavorite platform.\n\nFor example:\n  ng add @angular/fire\n  ng add @azure/ng-deploy\n  ng add @zeit/ng-deploy\n\nFind more packages on npm https://www.npmjs.com/search?q=ng%20deploy\n";
+    readonly missingTargetError = "\nCannot find \"deploy\" target for the specified project.\n\nYou should add a package that implements deployment capabilities for your\nfavorite platform.\n\nFor example:\n  ng add @angular/fire\n  ng add @azure/ng-deploy\n\nFind more packages on npm https://www.npmjs.com/search?q=ng%20deploy\n";
     initialize(options: DeployCommandSchema & Arguments): Promise<number | void>;
 }
