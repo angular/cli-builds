e255263e16008a17accb1ab9ff07be8d91c1c164
Angular Builds <circleci@angular.io>
2022-01-28 20:42:52 +0000

    e849cded1 DO NOT SUBMIT THIS OR ELSE

--- commands/serve.d.ts
+++ commands/serve.d.ts
@@ -8,20 +8,12 @@ export interface Schema {
      * The builder uses the named configurations to run the given target.
      * For more information, see
      * https://angular.io/guide/workspace-config#alternate-build-configurations.
-     * Setting this explicitly overrides the "--prod" flag.
      */
     configuration?: string;
     /**
      * Shows a help message for this command in the console.
      */
     help?: HelpUnion;
-    /**
-     * Shorthand for "--configuration=production".
-     * Set the build configuration to the production target.
-     * By default, the production target is set up in the workspace configuration such that all
-     * builds make use of bundling, limited tree-shaking, and also limited dead code elimination.
-     */
-    prod?: boolean;
     /**
      * The name of the project to build. Can be an application or a library.
      */
