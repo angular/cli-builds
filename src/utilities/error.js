0e9a7cb6c52b7e197c16be10604d05d00ea74f71
Angular Builds <circleci@angular.io>
2022-06-10 13:58:14 +0000

    e2c750273 test: split module resolutions E2E in multiple tests

---
+++ src/utilities/error.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.assertIsError = void 0;
+const assert_1 = __importDefault(require("assert"));
+function assertIsError(value) {
+    (0, assert_1.default)(value instanceof Error, 'catch clause variable is not an Error instance');
+}
+exports.assertIsError = assertIsError;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXJyb3IuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi8uLi8uLi8uLi9wYWNrYWdlcy9hbmd1bGFyL2NsaS9zcmMvdXRpbGl0aWVzL2Vycm9yLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQTs7Ozs7O0dBTUc7Ozs7OztBQUVILG9EQUE0QjtBQUU1QixTQUFnQixhQUFhLENBQUMsS0FBYztJQUMxQyxJQUFBLGdCQUFNLEVBQUMsS0FBSyxZQUFZLEtBQUssRUFBRSxnREFBZ0QsQ0FBQyxDQUFDO0FBQ25GLENBQUM7QUFGRCxzQ0FFQyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogQGxpY2Vuc2VcbiAqIENvcHlyaWdodCBHb29nbGUgTExDIEFsbCBSaWdodHMgUmVzZXJ2ZWQuXG4gKlxuICogVXNlIG9mIHRoaXMgc291cmNlIGNvZGUgaXMgZ292ZXJuZWQgYnkgYW4gTUlULXN0eWxlIGxpY2Vuc2UgdGhhdCBjYW4gYmVcbiAqIGZvdW5kIGluIHRoZSBMSUNFTlNFIGZpbGUgYXQgaHR0cHM6Ly9hbmd1bGFyLmlvL2xpY2Vuc2VcbiAqL1xuXG5pbXBvcnQgYXNzZXJ0IGZyb20gJ2Fzc2VydCc7XG5cbmV4cG9ydCBmdW5jdGlvbiBhc3NlcnRJc0Vycm9yKHZhbHVlOiB1bmtub3duKTogYXNzZXJ0cyB2YWx1ZSBpcyBFcnJvciAmIHsgY29kZT86IHN0cmluZyB9IHtcbiAgYXNzZXJ0KHZhbHVlIGluc3RhbmNlb2YgRXJyb3IsICdjYXRjaCBjbGF1c2UgdmFyaWFibGUgaXMgbm90IGFuIEVycm9yIGluc3RhbmNlJyk7XG59XG4iXX0=
