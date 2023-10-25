d3385960c5afe474a23d6f7147677de049eeb91a
Angular Builds <circleci@angular.io>
2023-10-25 11:01:55 +0000

    58c06d4d2 build: update ossf/scorecard-action action to v2.3.1

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.1.0-next.0+sha-9768c18');
+// export const VERSION = new Version('17.1.0-next.0+sha-58c06d4');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
