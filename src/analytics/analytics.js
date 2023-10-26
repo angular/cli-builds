cbf8e5080545586cef67ff7710c4a0714dbc1ab7
Angular Builds <circleci@angular.io>
2023-10-26 14:21:54 +0000

    a0f8fac4b refactor: update `inquirer` to version `9.0.6`

--- src/analytics/analytics.js
+++ src/analytics/analytics.js
@@ -6,29 +6,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAnalyticsInfoString = exports.getAnalyticsUserId = exports.promptAnalytics = exports.setAnalyticsConfig = exports.isPackageNameSafeForAnalytics = exports.analyticsPackageSafelist = void 0;
 const core_1 = require("@angular-devkit/core");
@@ -36,6 +13,7 @@ const crypto_1 = require("crypto");
 const color_1 = require("../utilities/color");
 const config_1 = require("../utilities/config");
 const environment_options_1 = require("../utilities/environment-options");
+const load_esm_1 = require("../utilities/load-esm");
 const tty_1 = require("../utilities/tty");
 /* eslint-disable no-console */
 /**
@@ -89,8 +67,8 @@ async function promptAnalytics(context, global, force = false) {
         throw new Error(`Could not find a ${level} workspace. Are you in a project?`);
     }
     if (force || (0, tty_1.isTTY)()) {
-        const { prompt } = await Promise.resolve().then(() => __importStar(require('inquirer')));
-        const answers = await prompt([
+        const { default: inquirer } = await (0, load_esm_1.loadEsmModule)('inquirer');
+        const answers = await inquirer.prompt([
             {
                 type: 'confirm',
                 name: 'analytics',
--- src/command-builder/schematics-command-module.js
+++ src/command-builder/schematics-command-module.js
@@ -6,34 +6,11 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
 };
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
@@ -48,6 +25,7 @@ const analytics_1 = require("../analytics/analytics");
 const analytics_parameters_1 = require("../analytics/analytics-parameters");
 const config_1 = require("../utilities/config");
 const error_1 = require("../utilities/error");
+const load_esm_1 = require("../utilities/load-esm");
 const memoize_1 = require("../utilities/memoize");
 const tty_1 = require("../utilities/tty");
 const command_module_1 = require("./command-module");
@@ -203,8 +181,8 @@ class SchematicsCommandModule extends command_module_1.CommandModule {
                     return question;
                 });
                 if (questions.length) {
-                    const { prompt } = await Promise.resolve().then(() => __importStar(require('inquirer')));
-                    return prompt(questions);
+                    const { default: inquirer } = await (0, load_esm_1.loadEsmModule)('inquirer');
+                    return inquirer.prompt(questions);
                 }
                 else {
                     return {};
