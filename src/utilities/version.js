ba3355486134dd0b973272a712e6f669f2a0dd1f
Angular Builds <circleci@angular.io>
2023-10-30 09:14:19 +0000

    323289b8c test(@angular-devkit/build-angular): enable http-requests-assets e2e tests

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.0-rc.2+sha-d392d65');
+// export const VERSION = new Version('17.0.0-rc.2+sha-323289b');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
