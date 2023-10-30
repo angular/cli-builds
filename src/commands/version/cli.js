1c7e64f93d80d829c7389646f2212de708b4cb81
Angular Builds <circleci@angular.io>
2023-10-30 11:37:50 +0000

    91dcae16d fix(@angular/cli): add Node.js 20 as supported version

--- src/commands/version/cli.js
+++ src/commands/version/cli.js
@@ -18,7 +18,7 @@ const command_config_1 = require("../command-config");
 /**
  * Major versions of Node.js that are officially supported by Angular.
  */
-const SUPPORTED_NODE_MAJORS = [18];
+const SUPPORTED_NODE_MAJORS = [18, 20];
 const PACKAGE_PATTERNS = [
     /^@angular\/.*/,
     /^@angular-devkit\/.*/,
