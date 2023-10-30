a3f081968455d37add0743a18058a9f44fa68365
Angular Builds <circleci@angular.io>
2023-10-30 08:09:22 +0000

    7b1fd9cf3 refactor(@schematics/angular): update component schematic to use single `style` or `styleUrl` property

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.1.0-next.0+sha-0155ae7');
+// export const VERSION = new Version('17.1.0-next.0+sha-7b1fd9c');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
