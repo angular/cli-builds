af36da2e9dc3073f92f536f3aae4f778e52938d7
Angular Builds <circleci@angular.io>
2023-10-31 10:55:38 +0000

    486becd fix(@ngtools/webpack): remove setClassDebugInfo calls

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.0-rc.2+sha-056982f');
+// export const VERSION = new Version('17.0.0-rc.2+sha-486becd');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
