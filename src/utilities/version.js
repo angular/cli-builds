121de4663e80dc0879954039d6a132df2ab1f33f
Angular Builds <circleci@angular.io>
2023-10-31 14:39:24 +0000

    559e891 fix(@angular-devkit/build-angular): Windows Node.js 20 prerendering failure (#26186)

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.0-rc.2+sha-0d54f2d');
+// export const VERSION = new Version('17.0.0-rc.2+sha-559e891');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
