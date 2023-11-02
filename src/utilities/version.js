69e0128632d63539841b641731cc94f99c59c1a8
Angular Builds <circleci@angular.io>
2023-11-02 16:16:54 +0000

    98c46a0 test(@angular/cli): delay changes to reduce `tests/legacy-cli/e2e/tests/basic/rebuild.ts` flakiness

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.0-rc.3+sha-f4b6380');
+// export const VERSION = new Version('17.0.0-rc.3+sha-98c46a0');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
