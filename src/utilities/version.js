09b821f22a62c5584c539e8fc0479187c3bed439
Angular Builds <circleci@angular.io>
2023-11-07 19:57:45 +0000

    8a9def5 test: add timeout to vite re-use cache

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.0-rc.4+sha-9d4d11c');
+// export const VERSION = new Version('17.0.0-rc.4+sha-8a9def5');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
