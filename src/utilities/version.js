d111511fa3ac4eb5cba9c8f53f456691b84dbfd8
Angular Builds <circleci@angular.io>
2023-11-07 17:17:24 +0000

    569b714 build: lock file maintenance

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.0-rc.4+sha-455aed8');
+// export const VERSION = new Version('17.0.0-rc.4+sha-569b714');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
