644c4c0c05ee8d7b5f2ff2f842f3afb35d34a962
Angular Builds <circleci@angular.io>
2023-11-20 17:28:47 +0000

    d11b36f fix(@angular-devkit/build-angular): normalize paths in ssr sourcemaps to posix when using vite

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.1+sha-0236451');
+// export const VERSION = new Version('17.0.1+sha-d11b36f');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
