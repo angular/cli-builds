0089dd257e450e57a35e6aeb9bfb85ee04e81510
Angular Builds <circleci@angular.io>
2023-11-20 14:39:03 +0000

    0a9c2ab test: remove unneeded kill-all-processes calls in E2E tests

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.1.0-next.0+sha-a21046f');
+// export const VERSION = new Version('17.1.0-next.0+sha-0a9c2ab');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
