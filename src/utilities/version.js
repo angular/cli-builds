feb7bc09757e4c5a4a96dc00b611392e99af6898
Angular Builds <circleci@angular.io>
2023-11-23 16:45:09 +0000

    36d9f19 refactor(@angular-devkit/build-angular): remove no longer applicable comment

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.1.0-next.0+sha-1725b91');
+// export const VERSION = new Version('17.1.0-next.0+sha-36d9f19');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
