53c27d24d1a6f0229c1bc1adb69b832c304d0801
Angular Builds <circleci@angular.io>
2023-11-27 17:53:36 +0000

    22880d9 fix(@angular-devkit/build-angular): correct set locale when using esbuild based builders

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.3+sha-a068067');
+// export const VERSION = new Version('17.0.3+sha-22880d9');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
