b1ca9382a02f03327dc0caf228949b308b704679
Angular Builds <circleci@angular.io>
2023-12-11 14:49:38 +0000

    7b8d6cd fix(@angular-devkit/build-angular): handle updates of an `npm link` library from another workspace when `preserveSymlinks` is `true`

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.6+sha-9300248');
+// export const VERSION = new Version('17.0.6+sha-7b8d6cd');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
