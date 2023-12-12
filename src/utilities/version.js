3c0cae725d53ba932273398aebc5fc33f519c8d3
Angular Builds <circleci@angular.io>
2023-12-12 17:42:01 +0000

    f2f7d7c fix(@angular-devkit/build-angular): file is missing from the TypeScript compilation with JIT

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.6+sha-3623fe9');
+// export const VERSION = new Version('17.0.6+sha-f2f7d7c');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
