dd3a5056bdeae34441748593db10eff96c63c75b
Angular Builds <circleci@angular.io>
2023-12-13 14:03:24 +0000

    eeb5634 release: cut the v17.0.7 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.6+sha-f2f7d7c');
+// export const VERSION = new Version('17.0.7+sha-eeb5634');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
