db7045da177894d0a33be3710a33e0f1fe4536d1
Angular Builds <circleci@angular.io>
2024-01-03 15:21:46 +0000

    446dfb7 fix(@angular/cli): add prerender and ssr-dev-server schemas in angular.json schema

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.0.8+sha-88d6ca4');
+// export const VERSION = new Version('17.0.8+sha-446dfb7');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
