a41326f281f741051f4768582604aa763e61d358
Angular Builds <circleci@angular.io>
2024-02-09 14:39:55 +0000

    782c718 fix(@angular/cli): prevent BOM errors in `package.json` during `ng update`

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.3.0-next.0+sha-cc916c1');
+// export const VERSION = new Version('17.3.0-next.0+sha-782c718');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
