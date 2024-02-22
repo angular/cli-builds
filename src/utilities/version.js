a68467dfa2889bbcbcbb134d2c3fae02d88563d3
Angular Builds <circleci@angular.io>
2024-02-22 17:47:39 +0000

    48d468c build: update github/codeql-action action to v3.24.4

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.3.0-next.0+sha-9a6a77e');
+// export const VERSION = new Version('17.3.0-next.0+sha-48d468c');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
