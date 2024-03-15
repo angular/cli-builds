dd01f716a6386669c5937f96ee1c00833221940c
Angular Builds <circleci@angular.io>
2024-03-15 17:25:46 +0000

    cf17331 build: update ng-dev config to work with Node.js 18.19

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('17.3.0+sha-79c44ad');
+// export const VERSION = new Version('17.3.0+sha-cf17331');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
