00cdbda23e06201f8006f733455892eaf71929c8
Angular Builds <circleci@angular.io>
2024-03-20 23:48:49 +0000

    625c65b docs: release notes for the v17.3.1 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.0-next.0+sha-a82dc78');
+// export const VERSION = new Version('18.0.0-next.0+sha-625c65b');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
