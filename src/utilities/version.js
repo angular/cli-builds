acc51fd7473b0ed117524fdc20988b23682e91e2
Angular Builds <circleci@angular.io>
2024-03-21 13:34:07 +0000

    93f7a2a Revert "test: update e2e update test to include content in css file"

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.0-next.0+sha-d05f78b');
+// export const VERSION = new Version('18.0.0-next.0+sha-93f7a2a');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
