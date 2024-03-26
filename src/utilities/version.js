641f7ec74d391de209ad298148b0f5b79a61c9dd
Angular Builds <circleci@angular.io>
2024-03-26 16:18:59 +0000

    b454690 refactor(@angular-devkit/build-angular): update babel application preset with new plugin import location

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.0-next.0+sha-7682a71');
+// export const VERSION = new Version('18.0.0-next.0+sha-b454690');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
