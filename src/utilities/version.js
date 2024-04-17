7b3c7ab9f7bd4736ba29ed5217d499ebef2e51ca
Angular Builds <circleci@angular.io>
2024-04-17 17:33:18 +0000

    d51cb59 feat(@angular-devkit/build-angular): inject event-dispatch in SSR HTML page

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.0-next.2+sha-adf1197');
+// export const VERSION = new Version('18.0.0-next.2+sha-d51cb59');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
