ccbc734a11d500ac70112b02dfabfd3822e37807
Angular Builds <circleci@angular.io>
2024-04-30 14:20:32 +0000

    75b5023 build: update all non-major dependencies

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.0-next.5+sha-44b4017');
+// export const VERSION = new Version('18.0.0-next.5+sha-75b5023');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
