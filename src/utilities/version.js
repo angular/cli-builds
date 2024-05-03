e46973c6336492e72cfdee28a8ee37cf2101beba
Angular Builds <circleci@angular.io>
2024-05-03 16:21:40 +0000

    53cb8b9 build: replace lodash.template with lodash v4.17.21

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.0-rc.1+sha-12a3da7');
+// export const VERSION = new Version('18.0.0-rc.1+sha-53cb8b9');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
