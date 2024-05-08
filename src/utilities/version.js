4e3f1455b248565848b9a43579c3b03b1a685c87
Angular Builds <circleci@angular.io>
2024-05-08 07:00:53 +0000

    dbacadd refactor(@angular/build): show unexpected Sass import resolution errors

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.0-rc.1+sha-14c912c');
+// export const VERSION = new Version('18.0.0-rc.1+sha-dbacadd');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
