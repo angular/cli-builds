830191ea5e78da4b6a8a3a6cdca870d958c79e55
Angular Builds <circleci@angular.io>
2024-05-22 11:13:03 +0000

    dd786d4 fix(@angular/cli): only add --version option on default command

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.0-rc.3+sha-2aeecd6');
+// export const VERSION = new Version('18.0.0-rc.3+sha-dd786d4');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
