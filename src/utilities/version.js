a787df412d3cf4c32c8c4069c8d57d1a9b6bd8c6
Angular Builds <circleci@angular.io>
2024-05-31 19:29:13 +0000

    22e05dc fix(@schematics/angular): generate new projects with ECMAScript standard class field behavior

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.1.0-next.0+sha-86a0723');
+// export const VERSION = new Version('18.1.0-next.0+sha-22e05dc');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
