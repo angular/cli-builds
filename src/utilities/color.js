151944bd887786d2b4a17c9ff5b208bec974a147
Angular Builds <circleci@angular.dev>
2024-06-11 12:24:03 +0000

    9e35d17 refactor(@angular/cli): use Node.js builtin ANSI escape removal helper

--- src/utilities/color.js
+++ src/utilities/color.js
@@ -31,7 +31,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.colors = void 0;
-exports.removeColor = removeColor;
 const ansiColors = __importStar(require("ansi-colors"));
 const tty_1 = require("tty");
 function supportColor() {
@@ -57,11 +56,6 @@ function supportColor() {
         return process.stdout.getColorDepth() > 1;
     }
     return false;
-}
-function removeColor(text) {
-    // This has been created because when colors.enabled is false unstyle doesn't work
-    // see: https://github.com/doowb/ansi-colors/blob/a4794363369d7b4d1872d248fc43a12761640d8e/index.js#L38
-    return text.replace(ansiColors.ansiRegex, '');
 }
 // Create a separate instance to prevent unintended global changes to the color configuration
 const colors = ansiColors.create();
