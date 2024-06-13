1177a00dab3f0f75c1199b88a4c62e127a95b5f7
Angular Builds <circleci@angular.dev>
2024-06-13 20:04:12 +0000

    405c148 fix(@angular/build): automatically resolve `.mjs` files when using Vite

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.4+sha-df783bb');
+// export const VERSION = new Version('18.0.4+sha-405c148');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
