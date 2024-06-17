670155c40fc11fee6c2ee2f5c227613ff66dab59
Angular Builds <circleci@angular.dev>
2024-06-17 10:43:21 +0000

    5c705e8 fix(@schematics/angular): update schematics to use RouterModule when --routing flag is present

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.0.4+sha-405c148');
+// export const VERSION = new Version('18.0.4+sha-5c705e8');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
