105ed76d850ba9c247c284b0b63a239a3c793a25
Angular Builds <circleci@angular.dev>
2024-06-28 13:42:00 +0000

    9a1c059 fix(@angular/build): redirect to path with trailing slash for asset directories

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.2.0-next.0+sha-a5f1b91');
+// export const VERSION = new Version('18.2.0-next.0+sha-9a1c059');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
