233ef7ee1c60ddda3f4f0d30c150ec873dbe5244
Angular Builds <circleci@angular.dev>
2024-07-08 10:13:09 +0000

    1772c87 refactor(@angular/cli): provide a default extract-i18n target for applications

--- src/command-builder/architect-command-module.d.ts
+++ src/command-builder/architect-command-module.d.ts
@@ -5,6 +5,8 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
+import { Target } from '@angular-devkit/architect';
+import { workspaces } from '@angular-devkit/core';
 import { Argv } from 'yargs';
 import { ArchitectBaseCommandModule } from './architect-base-command-module';
 import { CommandModuleImplementation, Options, OtherOptions } from './command-module';
@@ -14,6 +16,7 @@ export interface ArchitectCommandArgs {
 }
 export declare abstract class ArchitectCommandModule extends ArchitectBaseCommandModule<ArchitectCommandArgs> implements CommandModuleImplementation<ArchitectCommandArgs> {
     abstract readonly multiTarget: boolean;
+    findDefaultBuilderName?(project: workspaces.ProjectDefinition, target: Target): Promise<string | undefined>;
     builder(argv: Argv): Promise<Argv<ArchitectCommandArgs>>;
     run(options: Options<ArchitectCommandArgs> & OtherOptions): Promise<number | void>;
     private getArchitectProject;
--- src/commands/extract-i18n/cli.d.ts
+++ src/commands/extract-i18n/cli.d.ts
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
+import { workspaces } from '@angular-devkit/core';
 import { ArchitectCommandModule } from '../../command-builder/architect-command-module';
 import { CommandModuleImplementation } from '../../command-builder/command-module';
 export default class ExtractI18nCommandModule extends ArchitectCommandModule implements CommandModuleImplementation {
@@ -12,4 +13,5 @@ export default class ExtractI18nCommandModule extends ArchitectCommandModule imp
     command: string;
     describe: string;
     longDescriptionPath?: string | undefined;
+    findDefaultBuilderName(project: workspaces.ProjectDefinition): Promise<string | undefined>;
 }
