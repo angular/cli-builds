647d63692667de04323ebf65e7b685c00cb9dff4
Angular Builds <circleci@angular.dev>
2024-07-19 16:56:46 +0000

    3e4ea77 fix(@angular/build): correctly detect comma in Sass URL lexer

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.1.1+sha-5b9378a');
+// export const VERSION = new Version('18.1.1+sha-3e4ea77');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
