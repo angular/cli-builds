18194f2ddc7eb1a8d2abea7cd83044ac94ed86ab
Angular Builds <circleci@angular.dev>
2024-08-22 18:48:51 +0000

    4aa95bd build: clean up redundant `exclude` field from `tsconfig-build.json`

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.0-next.1+sha-7ea85e9');
+// export const VERSION = new Version('19.0.0-next.1+sha-4aa95bd');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
