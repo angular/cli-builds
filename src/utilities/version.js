2985c958aa5451aad8d4a2faaab2401306a4e55c
Angular Builds <circleci@angular.dev>
2024-09-04 19:58:02 +0000

    f4db482 release: cut the v18.2.3 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.2.2+sha-4820766');
+// export const VERSION = new Version('18.2.3+sha-f4db482');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
