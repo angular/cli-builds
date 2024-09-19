889283399fd4e7938281dadfe4c33b446da7563e
Angular Builds <circleci@angular.dev>
2024-09-19 19:34:01 +0000

    3b00fc9 feat(@angular/build): introduce `outputMode` option to the application builder

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.0-next.7+sha-8723e0c');
+// export const VERSION = new Version('19.0.0-next.7+sha-3b00fc9');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
