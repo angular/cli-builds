87ec766e596e0a16174b6c40ec565d49ae51376e
Angular Builds <circleci@angular.dev>
2024-09-24 19:24:04 +0000

    c710539 test: update test logic to fix windows CI

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.2.5+sha-6a52dcc');
+// export const VERSION = new Version('18.2.5+sha-c710539');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
