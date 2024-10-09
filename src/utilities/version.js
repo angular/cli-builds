c3782be8260d49e2a0b1953fd6511d316bdb9fb8
Angular Builds <circleci@angular.dev>
2024-10-09 13:15:21 +0000

    9445916 fix(@angular/build): `Ctrl + C` not terminating dev-server with SSR

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.2.7+sha-9b5cfaa');
+// export const VERSION = new Version('18.2.7+sha-9445916');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
