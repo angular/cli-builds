1120d2a9e7cba7d66efccc104a1299f7e7f406cc
Angular Builds <circleci@angular.dev>
2024-10-10 20:25:25 +0000

    bfa8fec fix(@angular/build): use named export `reqHandler` for server.ts request handling

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.0-next.10+sha-01f7475');
+// export const VERSION = new Version('19.0.0-next.10+sha-bfa8fec');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
