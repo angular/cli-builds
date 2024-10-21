ea7e4a5dce1c8781e997841ca4476be9a0001993
Angular Builds <circleci@angular.dev>
2024-10-21 07:19:25 +0000

    44077f5 fix(@angular/ssr): designate package as side-effect free

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.0-next.11+sha-c38ff43');
+// export const VERSION = new Version('19.0.0-next.11+sha-44077f5');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
