300237c966f322fbe5d9a4d0af00201afa4f748b
Angular Builds <circleci@angular.dev>
2024-10-23 21:02:59 +0000

    aba630c release: cut the v19.0.0-next.13 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.0-next.12+sha-a602be7');
+// export const VERSION = new Version('19.0.0-next.13+sha-aba630c');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
