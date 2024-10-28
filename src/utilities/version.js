5900355c417180f000cd96bcd0320ce85114f431
Angular Builds <circleci@angular.dev>
2024-10-28 16:59:23 +0000

    1fc8fde refactor(@angular/ssr): remove custom Platform Server creation Logic

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.1.0-next.0+sha-b73b98d');
+// export const VERSION = new Version('19.1.0-next.0+sha-1fc8fde');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
