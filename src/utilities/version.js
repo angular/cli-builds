6e116bf3a0e7c69eb434d29904cd32de1eb3e499
Angular Builds <circleci@angular.dev>
2024-10-28 19:56:20 +0000

    c3f800c build: patch `rollup-license-plugin` to fix on windows

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.0-next.13+sha-da505d1');
+// export const VERSION = new Version('19.0.0-next.13+sha-c3f800c');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
