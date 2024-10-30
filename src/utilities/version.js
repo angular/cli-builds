736e31f1ad8c9da377363a1cdba29d029053abc7
Angular Builds <circleci@angular.dev>
2024-10-30 23:52:38 +0000

    a3e2bcd refactor(@angular/build): remove experimental `buildApplication` overload

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.1.0-next.0+sha-289567e');
+// export const VERSION = new Version('19.1.0-next.0+sha-a3e2bcd');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
