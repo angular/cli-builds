00a31c51b855bb9dbf1388b736ec3e209dedb564
Angular Builds <dev-infra@angular.dev>
2024-11-15 17:53:31 +0000

    d77527d fix(@angular/ssr): export PrerenderFallback

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.1.0-next.0+sha-2850318');
+// export const VERSION = new Version('19.1.0-next.0+sha-d77527d');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
