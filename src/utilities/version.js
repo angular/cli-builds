e4c7bc588ba5ed9a4650d20943d8017d3aede079
Angular Builds <dev-infra@angular.dev>
2024-11-21 07:25:34 +0000

    8bd2b26 fix(@angular/ssr): handle baseHref that start with `./`

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.0+sha-74461da');
+// export const VERSION = new Version('19.0.0+sha-8bd2b26');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
