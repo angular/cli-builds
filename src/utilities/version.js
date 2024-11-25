fbf5aa244237880c698789d6641520ab81b3822f
Angular Builds <dev-infra@angular.dev>
2024-11-25 20:09:12 +0000

    5320e87 refactor(@angular/build): maintain previous compiler plugin factory signature

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.1.0-next.0+sha-7b0aa3d');
+// export const VERSION = new Version('19.1.0-next.0+sha-5320e87');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
