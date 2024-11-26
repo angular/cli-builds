23a3073d7a9861f833a86515a6bc5cf7763389d9
Angular Builds <dev-infra@angular.dev>
2024-11-26 16:50:48 +0000

    277b8a3 fix(@angular/ssr): ensure compatibility for `Http2ServerResponse` type

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.2+sha-ab4e77c');
+// export const VERSION = new Version('19.0.2+sha-277b8a3');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
