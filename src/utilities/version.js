573c9029d49c77eb67a53e1083e74be7f129fb70
Angular Builds <dev-infra@angular.dev>
2024-12-02 09:06:08 +0000

    3d5f41f docs(@angular/cli): update angular-eslint reference (#29001)

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.2+sha-f26e1b4');
+// export const VERSION = new Version('19.0.2+sha-3d5f41f');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
