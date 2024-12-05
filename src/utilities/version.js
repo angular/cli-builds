b866a79eb36b9b01e7bedcb0e40525056939dfcb
Angular Builds <circleci@angular.dev>
2024-12-05 15:24:46 +0000

    0434eff ci: update dev-infra sha to use latest Bazel keys

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.2.12+sha-53b9e35');
+// export const VERSION = new Version('18.2.12+sha-0434eff');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
