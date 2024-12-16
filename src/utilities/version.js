dbd22335772d6421f872d11334547314fbfbb3bc
Angular Builds <dev-infra@angular.dev>
2024-12-16 14:07:22 +0000

    2f4df6b fix(@angular/ssr): correctly resolve pre-transform resources in Vite SSR without AppEngine

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.5+sha-db74212');
+// export const VERSION = new Version('19.0.5+sha-2f4df6b');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
