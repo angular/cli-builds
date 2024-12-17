6c245cb3c9573a5411b0d39346021062ea60f9bf
Angular Builds <dev-infra@angular.dev>
2024-12-17 06:34:45 +0000

    298b554 feat(@angular/build): enable component template hot replacement by default

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.1.0-next.1+sha-19bb2d4');
+// export const VERSION = new Version('19.1.0-next.1+sha-298b554');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
