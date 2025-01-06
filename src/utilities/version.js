921785067ad4ed6651fc33a8d327ac8c063ea4bd
Angular Builds <dev-infra@angular.dev>
2025-01-06 23:57:13 +0000

    1c011a2 ci: run less jobs per executor for RBE

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.1.0-next.2+sha-7b15a15');
+// export const VERSION = new Version('19.1.0-next.2+sha-1c011a2');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
