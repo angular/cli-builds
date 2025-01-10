b01e786f17be0ade9c33a61983bfbd576faa549c
Angular Builds <dev-infra@angular.dev>
2025-01-10 19:16:34 +0000

    217939a test: remove redundant e2e tests

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('19.0.7+sha-e41d8ff');
+// export const VERSION = new Version('19.0.7+sha-217939a');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
