f6f7a79ef65a3fd4f590ba8df586f00c475e6430
Angular Builds <circleci@angular.dev>
2025-03-13 09:46:44 +0000

    c88727a ci: update angular/dev-infra actions version

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.2.14+sha-d621da8');
+// export const VERSION = new Version('18.2.14+sha-c88727a');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
