d7503958762cee82be80b5a3b3c2e3d8ef8fa877
Angular Builds <circleci@angular.dev>
2025-03-26 08:33:28 +0000

    9c2904d fix(@angular/build): update vite to 5.4.15

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -25,5 +25,5 @@ class Version {
     }
 }
 // TODO(bazel): Convert this to use build-time version stamping after flipping the build script to use bazel
-// export const VERSION = new Version('18.2.15+sha-4267a80');
+// export const VERSION = new Version('18.2.15+sha-9c2904d');
 exports.VERSION = new Version(JSON.parse((0, fs_1.readFileSync)((0, path_1.resolve)(__dirname, '../../package.json'), 'utf-8')).version);
