9220ccf81d8bc6954958629f7f916f46ec13ed2c
Angular Builds <dev-infra@angular.dev>
2025-08-19 15:11:21 +0000

    f54cdff build: add missing `@angular/create` package.json to `npm_translate_lock` data

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,4 +22,4 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.0+sha-0505f95');
+exports.VERSION = new Version('21.0.0-next.0+sha-f54cdff-with-local-changes');
