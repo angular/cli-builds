b286ff8b507c56eb4caa709ccb731728e94e9765
Angular Builds <dev-infra@angular.dev>
2025-10-13 10:49:33 +0000

    a90bea5 feat(@angular/build): support `.test.ts` files by default in unit test builder

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.7+sha-8f0f6a5');
+exports.VERSION = new Version('21.0.0-next.7+sha-a90bea5');
 //# sourceMappingURL=version.js.map
