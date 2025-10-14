8a3b96e20a380020d2ffc5932856db582c444ccc
Angular Builds <dev-infra@angular.dev>
2025-10-14 17:34:20 +0000

    92ddc42 test(@schematics/angular): add additional test coverage for jasmine-to-vitest schematic

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.7+sha-629f5cb');
+exports.VERSION = new Version('21.0.0-next.7+sha-92ddc42');
 //# sourceMappingURL=version.js.map
