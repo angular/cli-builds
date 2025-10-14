ffbdb2f7ff122d8164a975be6b7ece74e438510f
Angular Builds <dev-infra@angular.dev>
2025-10-14 13:09:36 +0000

    f8c0744 refactor(@angular/ssr): handle promise-like results from `loadChildrenHelper`

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.7+sha-e7d955b');
+exports.VERSION = new Version('21.0.0-next.7+sha-f8c0744');
 //# sourceMappingURL=version.js.map
