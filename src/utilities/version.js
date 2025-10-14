387990fe1209742def36a6e4718394caef17e9de
Angular Builds <dev-infra@angular.dev>
2025-10-14 15:28:52 +0000

    629f5cb feat(@schematics/angular): add misc transformations to jasmine-to-vitest schematic

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.7+sha-d6830d2');
+exports.VERSION = new Version('21.0.0-next.7+sha-629f5cb');
 //# sourceMappingURL=version.js.map
