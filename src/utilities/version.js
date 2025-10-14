da0e61c9e2fed4e5a6f9f5a83072d48a72a4580e
Angular Builds <dev-infra@angular.dev>
2025-10-14 14:13:28 +0000

    d0d2a17 feat(@schematics/angular): add Jasmine spy API transformations to jasmine-to-vitest schematic

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.7+sha-ad113c7');
+exports.VERSION = new Version('21.0.0-next.7+sha-d0d2a17');
 //# sourceMappingURL=version.js.map
