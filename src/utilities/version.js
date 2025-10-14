fe015b601a20e36a637c18092dc571ae3c66f92f
Angular Builds <dev-infra@angular.dev>
2025-10-14 19:35:40 +0000

    ba18bcc refactor(@schematics/angular): remove duplicate jasmine-to-vitest transformers

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.7+sha-92ddc42');
+exports.VERSION = new Version('21.0.0-next.7+sha-ba18bcc');
 //# sourceMappingURL=version.js.map
