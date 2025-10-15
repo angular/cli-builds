8c2f95904ca04e8fb78b0971ae7648c85a45f2e8
Angular Builds <dev-infra@angular.dev>
2025-10-15 11:35:08 +0000

    00b7d74 docs(@angular-devkit/build-angular): add descriptions for browser builder options

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.7+sha-b72fa91');
+exports.VERSION = new Version('21.0.0-next.7+sha-00b7d74');
 //# sourceMappingURL=version.js.map
