ce542e78b8a2db6f69da3ecf6484a840e347b283
Angular Builds <dev-infra@angular.dev>
2025-10-22 15:00:47 +0000

    2761d85 docs: update missing developer doc link

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.8+sha-396e5c4');
+exports.VERSION = new Version('21.0.0-next.8+sha-2761d85');
 //# sourceMappingURL=version.js.map
