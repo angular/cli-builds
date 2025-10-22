11449ef578178192f9b771207e677a140315c625
Angular Builds <dev-infra@angular.dev>
2025-10-22 01:09:43 +0000

    bf468e1 fix(@angular/build): direct check include file exists in unit-test discovery

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.8+sha-122a8c0');
+exports.VERSION = new Version('21.0.0-next.8+sha-bf468e1');
 //# sourceMappingURL=version.js.map
