d533d9b28ea90be858f3cb68e3af4c78ea4a23b9
Angular Builds <dev-infra@angular.dev>
2025-10-22 16:50:28 +0000

    b524ba4 fix(@schematics/angular): remove empty i18n-extract target for new projects

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.8+sha-220961f');
+exports.VERSION = new Version('21.0.0-next.8+sha-b524ba4');
 //# sourceMappingURL=version.js.map
