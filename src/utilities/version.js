23a783ddadcb748521d54f9961f5f800e79bf65e
Angular Builds <dev-infra@angular.dev>
2025-10-22 16:26:08 +0000

    220961f build(@angular/build): update vitest to v4.0.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.8+sha-2761d85');
+exports.VERSION = new Version('21.0.0-next.8+sha-220961f');
 //# sourceMappingURL=version.js.map
