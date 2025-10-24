d7add94a55c73d358d3daba6bf49bb0090c7cf49
Angular Builds <dev-infra@angular.dev>
2025-10-24 10:53:54 +0000

    1dfb8c2 build: update dependency ini to v6

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-49f12fd');
+exports.VERSION = new Version('21.1.0-next.0+sha-1dfb8c2');
 //# sourceMappingURL=version.js.map
