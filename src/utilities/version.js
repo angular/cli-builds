a301498a456b88e31b1785cb3247471dd9f725cc
Angular Builds <dev-infra@angular.dev>
2025-10-24 11:58:50 +0000

    f2248ba fix(@angular/build): add --ui option for Vitest runner

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-1dfb8c2');
+exports.VERSION = new Version('21.1.0-next.0+sha-f2248ba');
 //# sourceMappingURL=version.js.map
