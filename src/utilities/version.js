a0271ca0704cf34830057659c3d72eb93f348594
Angular Builds <dev-infra@angular.dev>
2025-10-27 17:17:00 +0000

    20079ed fix(@angular/build): allow custom runner configuration file for unit-test

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-ae6a12f');
+exports.VERSION = new Version('21.1.0-next.0+sha-20079ed');
 //# sourceMappingURL=version.js.map
