98211530efdfdd905b6146c09c95ef01c85e0ad5
Angular Builds <dev-infra@angular.dev>
2025-10-27 16:32:08 +0000

    b884eb8 build: update rules_angular digest to 1f1aa3e

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.9+sha-900a84d');
+exports.VERSION = new Version('21.0.0-next.9+sha-b884eb8');
 //# sourceMappingURL=version.js.map
