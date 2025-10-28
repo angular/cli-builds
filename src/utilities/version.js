0aca05096581434aea778a652da85725f73bdefc
Angular Builds <dev-infra@angular.dev>
2025-10-28 11:55:32 +0000

    6dfee71 refactor(@schematics/angular): add trailing comma to generated services

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-2bcd188');
+exports.VERSION = new Version('21.1.0-next.0+sha-6dfee71');
 //# sourceMappingURL=version.js.map
