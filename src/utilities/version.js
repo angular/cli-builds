28327552fb88850e80df827e69db83fc1bdd1a00
Angular Builds <dev-infra@angular.dev>
2025-10-29 12:11:59 +0000

    9132e6a fix(@angular/build): resolve browser provider packages using project resolver

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.9+sha-9f20af1');
+exports.VERSION = new Version('21.0.0-next.9+sha-9132e6a');
 //# sourceMappingURL=version.js.map
