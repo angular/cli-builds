7126b18eea9bf1fb0682f689140ff77c08de256c
Angular Builds <dev-infra@angular.dev>
2025-10-29 16:05:32 +0000

    f8983bd build: update all non-major dependencies to v4.0.4

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-f2f121f');
+exports.VERSION = new Version('21.1.0-next.0+sha-f8983bd');
 //# sourceMappingURL=version.js.map
