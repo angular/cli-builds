efbbaf6065e3cb3b5b58d9a837a691ba32e55f76
Angular Builds <dev-infra@angular.dev>
2025-10-29 10:12:21 +0000

    163189c build: update dependency node to v22.21.1

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-cc16b10');
+exports.VERSION = new Version('21.1.0-next.0+sha-163189c');
 //# sourceMappingURL=version.js.map
