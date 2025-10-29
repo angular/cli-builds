b14e790385740684448d42a6d911fae700ef8921
Angular Builds <dev-infra@angular.dev>
2025-10-29 18:18:50 +0000

    3b536d9 refactor(@schematics/angular): add trailing commas to generated directive, pipe, and guard files

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-0fe6db9');
+exports.VERSION = new Version('21.1.0-next.0+sha-3b536d9');
 //# sourceMappingURL=version.js.map
