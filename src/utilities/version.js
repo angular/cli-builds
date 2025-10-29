4dbf308d7003cda6220882cfb950a401b3b4e9ea
Angular Builds <dev-infra@angular.dev>
2025-10-29 16:47:21 +0000

    4211534 fix(@angular/build): add `define` option to dev-server

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-f8983bd');
+exports.VERSION = new Version('21.1.0-next.0+sha-4211534');
 //# sourceMappingURL=version.js.map
