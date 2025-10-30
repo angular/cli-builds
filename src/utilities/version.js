c3c38de9f7e9f8e4f3fa23ad603067f7b92a59bb
Angular Builds <dev-infra@angular.dev>
2025-10-30 09:46:31 +0000

    a85efb9 release: cut the v21.0.0-rc.0 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-next.10+sha-953c75a');
+exports.VERSION = new Version('21.0.0-rc.0+sha-a85efb9');
 //# sourceMappingURL=version.js.map
