bbfccefc85116ad7949166a8eeea40c47f00d82a
Angular Builds <dev-infra@angular.dev>
2025-10-31 19:12:12 +0000

    ce3fa2c refactor(@angular/cli): Change modernize to use a host interface for OS/FS operations

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-3a37cf6');
+exports.VERSION = new Version('21.1.0-next.0+sha-ce3fa2c');
 //# sourceMappingURL=version.js.map
