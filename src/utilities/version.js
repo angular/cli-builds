97680042c8362baa985c6f1d4970ebb1526849f0
Angular Builds <dev-infra@angular.dev>
2025-11-03 21:58:41 +0000

    dfb4242 fix(@angular/cli): add vitest to version command output

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.0+sha-8694369');
+exports.VERSION = new Version('21.0.0-rc.0+sha-dfb4242');
 //# sourceMappingURL=version.js.map
