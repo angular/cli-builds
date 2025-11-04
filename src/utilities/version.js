12ae7a50e7677b7ffa8a72934077f15bbc8d9361
Angular Builds <dev-infra@angular.dev>
2025-11-04 10:30:24 +0000

    62938e7 fix(@angular/build): update vitest to 4.0.6 and remove coverage workaround

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.0+sha-5d30887');
+exports.VERSION = new Version('21.0.0-rc.0+sha-62938e7');
 //# sourceMappingURL=version.js.map
