b455d676dbfc5cc78cb132000ec4b0b5c7de705e
Angular Builds <dev-infra@angular.dev>
2025-11-04 14:52:54 +0000

    4eb22bc fix(@angular/ssr): improve route matching for wildcard routes

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-b5f4192');
+exports.VERSION = new Version('21.1.0-next.0+sha-4eb22bc');
 //# sourceMappingURL=version.js.map
