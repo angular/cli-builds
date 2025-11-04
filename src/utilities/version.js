2bd169ab3d486830af2008b259509fac40713d49
Angular Builds <dev-infra@angular.dev>
2025-11-04 10:27:23 +0000

    b5f4192 fix(@schematics/angular): add warnings and improve Karma config generation

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-9268cf3');
+exports.VERSION = new Version('21.1.0-next.0+sha-b5f4192');
 //# sourceMappingURL=version.js.map
