b38f6556a71c73a892302adacf3f49d4042d5f12
Angular Builds <dev-infra@angular.dev>
2025-11-04 15:39:39 +0000

    d5bae29 docs: clarify `outputMode` description in application schema

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.0+sha-4a3cfdf');
+exports.VERSION = new Version('21.0.0-rc.0+sha-d5bae29');
 //# sourceMappingURL=version.js.map
