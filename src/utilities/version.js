413511193ce3d12eddc81549b3b95fc39a1d5a6e
Angular Builds <dev-infra@angular.dev>
2025-11-04 21:27:12 +0000

    f4895d6 refactor(@schematics/angular): improve code quality in jasmine-vitest transformer

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-cc132e9');
+exports.VERSION = new Version('21.1.0-next.0+sha-f4895d6');
 //# sourceMappingURL=version.js.map
