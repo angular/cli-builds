7b46a6277cb501b64f263195763509a4ee22463f
Angular Builds <dev-infra@angular.dev>
2025-11-05 22:36:06 +0000

    63c9874 fix(@angular/build): add webcontainer support for Vitest browser provider

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.1+sha-1be35b3');
+exports.VERSION = new Version('21.0.0-rc.1+sha-63c9874');
 //# sourceMappingURL=version.js.map
