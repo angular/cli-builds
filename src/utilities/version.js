d8962dcdf3c77876443d6f23079c0c9eb3904282
Angular Builds <dev-infra@angular.dev>
2025-11-06 12:32:59 +0000

    d2bd7f6 refactor(@angular/build): remove advanced chunking option in experimental chunk optimizer

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-50072cd');
+exports.VERSION = new Version('21.1.0-next.0+sha-d2bd7f6');
 //# sourceMappingURL=version.js.map
