b97d052ae5a85a3d9dd3f82b7c1a92d55907f182
Angular Builds <dev-infra@angular.dev>
2025-11-06 20:31:53 +0000

    0497b01 build: update rolldown to v1.0.0-beta.47

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.1+sha-0851d2e');
+exports.VERSION = new Version('21.0.0-rc.1+sha-0497b01');
 //# sourceMappingURL=version.js.map
