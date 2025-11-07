4e5f4271fd7cd7c112580e5f937b3a5d6a7bedf1
Angular Builds <dev-infra@angular.dev>
2025-11-07 10:58:24 +0000

    a8465d4 refactor: fix several typos (#31716)

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-c3c9ac5');
+exports.VERSION = new Version('21.1.0-next.0+sha-a8465d4');
 //# sourceMappingURL=version.js.map
