4639fc02359cd2a3581658e34421bed6f6c3855d
Angular Builds <dev-infra@angular.dev>
2025-11-08 10:35:15 +0000

    a44f8fa fix(@angular/build): dynamically select Vitest DOM environment

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.1+sha-cc2668f');
+exports.VERSION = new Version('21.0.0-rc.1+sha-a44f8fa');
 //# sourceMappingURL=version.js.map
