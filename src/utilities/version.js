076842f72d9a1e25124bb910dfe5a7ba79802e90
Angular Builds <dev-infra@angular.dev>
2025-11-08 08:37:27 +0000

    cc2668f fix(@angular/build): simplify SSL handling for `ng serve` with SSR (#31723)

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.1+sha-b76962d');
+exports.VERSION = new Version('21.0.0-rc.1+sha-cc2668f');
 //# sourceMappingURL=version.js.map
