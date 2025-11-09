1ed69fa3ee5ec4c2f629fac00d53b4d66266151b
Angular Builds <dev-infra@angular.dev>
2025-11-09 08:22:19 +0000

    0aab115 fix(@angular/build): enhance Vitest config merging and validation

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-53bdbf1');
+exports.VERSION = new Version('21.1.0-next.0+sha-0aab115');
 //# sourceMappingURL=version.js.map
