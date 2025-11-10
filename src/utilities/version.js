92877f278ddffdf8e83093b81bb7d6f30696f304
Angular Builds <dev-infra@angular.dev>
2025-11-10 07:41:03 +0000

    377780a fix(@angular/build): ensure TestBed setup is robust in non-isolated Vitest

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-0aab115');
+exports.VERSION = new Version('21.1.0-next.0+sha-377780a');
 //# sourceMappingURL=version.js.map
