6f4eec5b70374f3340a7f45f619761019dfa28f4
Angular Builds <dev-infra@angular.dev>
2025-11-11 18:48:46 +0000

    a9acc2e fix(@angular/cli): add schema versioning and metadata to example database

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-42a6da9');
+exports.VERSION = new Version('21.1.0-next.0+sha-a9acc2e');
 //# sourceMappingURL=version.js.map
