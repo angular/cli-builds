605455dfc99bfc43454f6640a9498801c86dde48
Angular Builds <dev-infra@angular.dev>
2025-11-11 16:30:52 +0000

    b65d22e build: update dependency jsdom to v27.1.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.1+sha-ee6338b');
+exports.VERSION = new Version('21.0.0-rc.1+sha-b65d22e');
 //# sourceMappingURL=version.js.map
