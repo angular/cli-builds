d146680e755d3d618508f367cfe485c9f158af86
Angular Builds <dev-infra@angular.dev>
2025-11-13 17:47:05 +0000

    6f2d4a9 refactor(@angular/build): remove deprecated browser-level isolate option

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-d74626f');
+exports.VERSION = new Version('21.1.0-next.0+sha-6f2d4a9');
 //# sourceMappingURL=version.js.map
