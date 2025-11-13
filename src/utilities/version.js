c9d96455c5089c3219fa405fc747f33a68d38b44
Angular Builds <dev-infra@angular.dev>
2025-11-13 17:52:50 +0000

    4deac3e fix(@angular/cli): support multi-database search in find_examples MCP tool

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.2+sha-1c3f562');
+exports.VERSION = new Version('21.0.0-rc.2+sha-4deac3e');
 //# sourceMappingURL=version.js.map
