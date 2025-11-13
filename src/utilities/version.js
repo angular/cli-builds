604ef1f47027cc97364fe4a128ded1ba7796f8ef
Angular Builds <dev-infra@angular.dev>
2025-11-13 19:35:18 +0000

    138649e fix(@angular/build): remove explicit test isolation configuration

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-6f2d4a9');
+exports.VERSION = new Version('21.1.0-next.0+sha-138649e');
 //# sourceMappingURL=version.js.map
