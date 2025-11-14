d6ea10513ca9c1c51f04ede3bc0800ccdbbd20cf
Angular Builds <dev-infra@angular.dev>
2025-11-14 20:45:36 +0000

    62a5256 ci: bump specs for e2e-package-managers

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.3+sha-7a781d4');
+exports.VERSION = new Version('21.0.0-rc.3+sha-62a5256');
 //# sourceMappingURL=version.js.map
