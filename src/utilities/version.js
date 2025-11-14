c73f0f68f91fd7c0a02c5b20e0091b7155a953c8
Angular Builds <dev-infra@angular.dev>
2025-11-14 00:19:49 +0000

    8bde4a3 test: stabilize Vitest snapshot E2E test on Windows

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-138649e');
+exports.VERSION = new Version('21.1.0-next.0+sha-8bde4a3');
 //# sourceMappingURL=version.js.map
