3e3f86d4b234d477818becc1d158422da2017b4f
Angular Builds <dev-infra@angular.dev>
2025-11-15 02:19:53 +0000

    49b65ab fix(@angular/build): disable Vitest test isolation by default

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.4+sha-6b2a88a');
+exports.VERSION = new Version('21.0.0-rc.4+sha-49b65ab');
 //# sourceMappingURL=version.js.map
