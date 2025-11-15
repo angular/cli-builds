f40bd556396a200b961ef6f205a2e5ff70b629bf
Angular Builds <dev-infra@angular.dev>
2025-11-15 13:32:22 +0000

    960c80c test(@angular/build): add e2e for a larger project with Vitest

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-4d42a3e');
+exports.VERSION = new Version('21.1.0-next.0+sha-960c80c');
 //# sourceMappingURL=version.js.map
