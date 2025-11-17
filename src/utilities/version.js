35394fcbf97c11c643434f0fa3ff9b7e43bca4ef
Angular Builds <dev-infra@angular.dev>
2025-11-17 12:30:33 +0000

    c241dd8 fix(@angular/build): configure Vitest cache to use Angular cache

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-14cd1e1');
+exports.VERSION = new Version('21.1.0-next.0+sha-c241dd8');
 //# sourceMappingURL=version.js.map
