3fbd485a9479f12054e39c64e220b77b82a4db1b
Angular Builds <dev-infra@angular.dev>
2025-11-17 17:30:17 +0000

    70e67fe fix(@schematics/angular): add missing typeSeparator to main.ts.template file

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-102ca8b');
+exports.VERSION = new Version('21.1.0-next.0+sha-70e67fe');
 //# sourceMappingURL=version.js.map
