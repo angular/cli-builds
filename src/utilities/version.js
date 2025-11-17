740b882c67c0e1934a8f4e6a452a44e8ecd98e29
Angular Builds <dev-infra@angular.dev>
2025-11-17 23:32:27 +0000

    8bd7f28 docs: release notes for the v21.0.0-rc.5 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-ed700a1');
+exports.VERSION = new Version('21.1.0-next.0+sha-8bd7f28');
 //# sourceMappingURL=version.js.map
