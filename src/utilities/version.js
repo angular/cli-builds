4ead7bce3f4e5117656ad124be706ae2aaf64aab
Angular Builds <dev-infra@angular.dev>
2025-11-17 11:52:09 +0000

    f05ffd1 fix(@angular/build): correct Vitest coverage include handling for virtual files

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0-rc.4+sha-bb29254');
+exports.VERSION = new Version('21.0.0-rc.4+sha-f05ffd1');
 //# sourceMappingURL=version.js.map
