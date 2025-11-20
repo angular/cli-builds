a0b5a81d48a038969e7448ebfc1ac19a2d70069c
Angular Builds <dev-infra@angular.dev>
2025-11-20 14:15:31 +0000

    a71411f fix(@angular/build): correctly configure per-browser headless mode in Vitest runner

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-0fe572e');
+exports.VERSION = new Version('21.1.0-next.0+sha-a71411f');
 //# sourceMappingURL=version.js.map
