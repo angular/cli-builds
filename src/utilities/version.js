c41193ec3ffa543a2886c7c6937de53f7644d276
Angular Builds <dev-infra@angular.dev>
2025-11-21 17:44:45 +0000

    59319b8 build: update bazel dependencies

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-18cf6c5');
+exports.VERSION = new Version('21.1.0-next.0+sha-59319b8');
 //# sourceMappingURL=version.js.map
