cea7fc3afec55503d01d83c2440a890edfc20f38
Angular Builds <dev-infra@angular.dev>
2025-11-21 08:14:53 +0000

    61a027d fix(@angular/ssr): prevent redirect loop with encoded query parameters

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-d134fa1');
+exports.VERSION = new Version('21.1.0-next.0+sha-61a027d');
 //# sourceMappingURL=version.js.map
