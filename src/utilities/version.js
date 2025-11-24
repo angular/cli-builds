9079039cef857408bef08d392293fb803057dcaf
Angular Builds <dev-infra@angular.dev>
2025-11-24 15:32:39 +0000

    16d898e fix(@schematics/angular): fix migration of `jasmine.clock().mockDate()`

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0+sha-2f58705');
+exports.VERSION = new Version('21.0.0+sha-16d898e');
 //# sourceMappingURL=version.js.map
