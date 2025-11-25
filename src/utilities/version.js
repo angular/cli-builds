9937b388445530699f8c0e657bd9c3a64b247424
Angular Builds <dev-infra@angular.dev>
2025-11-25 15:49:07 +0000

    21c3eac fix(@schematics/angular): handle createSpyObj without base name on refactor-jasmine-vitest

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0+sha-7222b54');
+exports.VERSION = new Version('21.0.0+sha-21c3eac');
 //# sourceMappingURL=version.js.map
