06378e3a7d18fdfc15fc9a574e51aa75aff263e7
Angular Builds <dev-infra@angular.dev>
2025-11-26 15:22:37 +0000

    2bfcbc3 build: update all github actions

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.0+sha-39ab18c');
+exports.VERSION = new Version('21.0.0+sha-2bfcbc3');
 //# sourceMappingURL=version.js.map
