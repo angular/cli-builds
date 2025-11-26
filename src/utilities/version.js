bfe699e0e01282ce1d2ffab8484b27e4f887cc18
Angular Builds <dev-infra@angular.dev>
2025-11-26 18:03:38 +0000

    e71a72f feat(@schematics/angular): generate detailed migration report for `refactor-jasmine-vitest`

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-aeb3b3e');
+exports.VERSION = new Version('21.1.0-next.0+sha-e71a72f');
 //# sourceMappingURL=version.js.map
