bd175a72125d35287d4e5bae2a01b6ea85aa43c6
Angular Builds <dev-infra@angular.dev>
2025-12-01 08:37:39 +0000

    331373f build: update dependency chokidar to v5

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-dabe101');
+exports.VERSION = new Version('21.1.0-next.0+sha-331373f');
 //# sourceMappingURL=version.js.map
