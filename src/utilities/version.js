e5709a2a31a03dd58aaf6980fe76006f7401c6c7
Angular Builds <dev-infra@angular.dev>
2025-12-01 16:20:27 +0000

    63c3e3f fix(@angular/build): add filename truncation to test discovery

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.0+sha-2d0e105');
+exports.VERSION = new Version('21.1.0-next.0+sha-63c3e3f');
 //# sourceMappingURL=version.js.map
