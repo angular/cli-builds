85090ffd69adbb1be10122fcec4131e1d6ac1bb9
Angular Builds <dev-infra@angular.dev>
2025-12-05 17:41:19 +0000

    385165c fix(@angular/build): inject testing polyfills in Karma unit-test executor

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.1+sha-38b16ea');
+exports.VERSION = new Version('21.1.0-next.1+sha-385165c');
 //# sourceMappingURL=version.js.map
