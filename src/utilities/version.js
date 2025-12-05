bfbba6b9879b8bc9c65dfb742b6e4eeceb10dfaf
Angular Builds <dev-infra@angular.dev>
2025-12-05 14:06:22 +0000

    588f3b8 ci: force ipv4 resolutions first in Node.js

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.1+sha-7038ea1');
+exports.VERSION = new Version('21.1.0-next.1+sha-588f3b8');
 //# sourceMappingURL=version.js.map
