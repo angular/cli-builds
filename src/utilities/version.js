6f52c5d71904f2f03bbeab247be33ba15ff94911
Angular Builds <dev-infra@angular.dev>
2025-12-05 17:24:26 +0000

    38b16ea fix(@angular/build): ensure locale base href retains leading slash (#32040)

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.1+sha-0cc102e');
+exports.VERSION = new Version('21.1.0-next.1+sha-38b16ea');
 //# sourceMappingURL=version.js.map
