db38b541088fec50af07511ccae723edeab9a95c
Angular Builds <dev-infra@angular.dev>
2025-12-05 11:29:41 +0000

    9033d94 ci: force dev server host to IPv4 to resolve RBE resolution issues

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.1+sha-668c04c');
+exports.VERSION = new Version('21.1.0-next.1+sha-9033d94');
 //# sourceMappingURL=version.js.map
