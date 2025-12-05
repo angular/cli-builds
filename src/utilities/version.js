7d37c933ca375b61a213a741f7abadb4cab3a1f9
Angular Builds <dev-infra@angular.dev>
2025-12-05 18:05:36 +0000

    78f9013 test: remove RxJS from E2E process utility

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.1+sha-985aa18');
+exports.VERSION = new Version('21.1.0-next.1+sha-78f9013');
 //# sourceMappingURL=version.js.map
