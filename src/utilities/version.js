6c040ce03c7470635f7848066702910ca839ac7f
Angular Builds <dev-infra@angular.dev>
2025-12-08 16:16:17 +0000

    88c18ce fix(@angular/build): support NODE_EXTRA_CA_CERTS in SSR SSL plugin

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.2+sha-c3d70bc');
+exports.VERSION = new Version('21.0.2+sha-88c18ce');
 //# sourceMappingURL=version.js.map
