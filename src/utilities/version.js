4e69227092645b209bdeafff511540859e396a2f
Angular Builds <dev-infra@angular.dev>
2025-12-10 19:03:13 +0000

    c1f9b7f release: cut the v21.1.0-next.2 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.1+sha-43dd1ff');
+exports.VERSION = new Version('21.1.0-next.2+sha-c1f9b7f');
 //# sourceMappingURL=version.js.map
