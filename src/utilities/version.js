be32c8a6880855176727713dff5bb5e3ba5b9017
Angular Builds <dev-infra@angular.dev>
2025-12-10 08:32:42 +0000

    2a35d8a build: update dependency rules_cc to v0.2.14

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.2+sha-7b063de');
+exports.VERSION = new Version('21.0.2+sha-2a35d8a');
 //# sourceMappingURL=version.js.map
