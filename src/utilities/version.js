8c8e0ca354748e01f17deb0d6b0e7e2a4c3ce32e
Angular Builds <dev-infra@angular.dev>
2025-12-11 15:22:10 +0000

    85a28de fix(@schematics/angular): remove `inlineSources` from library tsconfig template

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.3+sha-deb4fff');
+exports.VERSION = new Version('21.0.3+sha-85a28de');
 //# sourceMappingURL=version.js.map
