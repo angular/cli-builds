fafac02ceda06a124b7f914d0a84ae5e700b47f8
Angular Builds <dev-infra@angular.dev>
2025-12-12 19:44:11 +0000

    e4eee62 refactor(@schematics/angular): update type usage to support isolated declarations

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.2+sha-7c7e6a6');
+exports.VERSION = new Version('21.1.0-next.2+sha-e4eee62');
 //# sourceMappingURL=version.js.map
