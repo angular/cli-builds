1814da6d24173573f12224b345c81bbb80b7a7b7
Angular Builds <dev-infra@angular.dev>
2025-12-12 17:06:11 +0000

    7c7e6a6 fix(@angular/build): conditionally manage Vitest UI option

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.2+sha-efd1180');
+exports.VERSION = new Version('21.1.0-next.2+sha-7c7e6a6');
 //# sourceMappingURL=version.js.map
