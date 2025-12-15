3cfaa48d076e9e2d3e414e0db93e4aa7301263af
Angular Builds <dev-infra@angular.dev>
2025-12-15 13:55:15 +0000

    eb48d71 refactor(@angular-devkit/schematics-cli): migrate argument parsing to node:util.parseArgs

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.2+sha-9744af1');
+exports.VERSION = new Version('21.1.0-next.2+sha-eb48d71');
 //# sourceMappingURL=version.js.map
