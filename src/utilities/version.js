4b921b32bbbbd061751b621494483e4150c5e658
Angular Builds <dev-infra@angular.dev>
2025-12-15 16:08:47 +0000

    f1ab1d3 refactor(@angular-devkit/architect-cli): replace ansi-colors with node:util.styleText

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.2+sha-eb48d71');
+exports.VERSION = new Version('21.1.0-next.2+sha-f1ab1d3');
 //# sourceMappingURL=version.js.map
