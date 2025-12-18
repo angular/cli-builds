b6c933b08b82cfec8b081e5f44e26309ce673d1d
Angular Builds <dev-infra@angular.dev>
2025-12-18 14:49:04 +0000

    0d2735c refactor: directly set `responseInit.status` by removing type assertion and TODO comment

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.3+sha-3b3afb1');
+exports.VERSION = new Version('21.0.3+sha-0d2735c');
 //# sourceMappingURL=version.js.map
