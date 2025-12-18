4efe6bf422f96fb9eb7312a520dc0f0f92b65ca9
Angular Builds <dev-infra@angular.dev>
2025-12-18 15:03:28 +0000

    64be635 test: await package install in architect CLI E2E

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.2+sha-d76239a');
+exports.VERSION = new Version('21.1.0-next.2+sha-64be635');
 //# sourceMappingURL=version.js.map
