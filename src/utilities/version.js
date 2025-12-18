c3c0bde149a6f5eaf7ef1da54d2da16646e5d024
Angular Builds <dev-infra@angular.dev>
2025-12-18 00:25:17 +0000

    6bbfa89 build: update github/codeql-action action to v4.31.9

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.3+sha-570ce8d');
+exports.VERSION = new Version('21.0.3+sha-6bbfa89');
 //# sourceMappingURL=version.js.map
