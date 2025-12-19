fd51ad6cb6f9026f5e2aab17711aba92ff5d8f16
Angular Builds <dev-infra@angular.dev>
2025-12-19 13:55:24 +0000

    54f0eb6 build: remove tools/baseline_browserslist from pnpm workspace

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-9b34c62');
+exports.VERSION = new Version('21.1.0-next.3+sha-54f0eb6');
 //# sourceMappingURL=version.js.map
