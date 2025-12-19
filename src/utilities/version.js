1f55120468ee631538d6aee7658e45346f463bb1
Angular Builds <dev-infra@angular.dev>
2025-12-19 17:37:18 +0000

    95e35e5 build: add pnpm extension with specified version and integrity

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-716ef9d');
+exports.VERSION = new Version('21.1.0-next.3+sha-95e35e5');
 //# sourceMappingURL=version.js.map
