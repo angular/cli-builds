c0db2988452e371ea39a28828e809a3701e043a1
Angular Builds <dev-infra@angular.dev>
2025-12-22 14:47:31 +0000

    ca84b29 test: initial protractor to puppeteer E2E conversion

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-95e35e5');
+exports.VERSION = new Version('21.1.0-next.3+sha-ca84b29');
 //# sourceMappingURL=version.js.map
