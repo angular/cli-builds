ca97ece161fa992aeaa76c999ae11121af9db6af
Angular Builds <dev-infra@angular.dev>
2025-12-22 16:16:06 +0000

    b969432 Revert "build: restore `ng-dev` script"

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-5f1f0d1');
+exports.VERSION = new Version('21.1.0-next.3+sha-b969432');
 //# sourceMappingURL=version.js.map
