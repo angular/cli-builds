e222c518d572d51ddae61ec47be5bc963b16c5c4
Angular Builds <dev-infra@angular.dev>
2026-01-05 14:51:18 +0000

    164e7db fix(@angular/build): resolve test files correctly on Windows when using non-C drives

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-316fca8');
+exports.VERSION = new Version('21.1.0-next.3+sha-164e7db');
 //# sourceMappingURL=version.js.map
