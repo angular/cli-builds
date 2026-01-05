2240ea440c6916f4a81be408be4ef135cee7b57b
Angular Builds <dev-infra@angular.dev>
2026-01-05 14:33:53 +0000

    d85d768 refactor(@angular/build): export compiler plugin and stylesheet options types

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-cb24c24');
+exports.VERSION = new Version('21.1.0-next.3+sha-d85d768');
 //# sourceMappingURL=version.js.map
