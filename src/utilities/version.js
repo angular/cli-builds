9dad31065453df529088eda30df2bc4ceb15504b
Angular Builds <dev-infra@angular.dev>
2026-01-07 14:01:06 +0000

    31132bc refactor: update copyright year in license

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-1e39c77');
+exports.VERSION = new Version('21.1.0-next.3+sha-31132bc');
 //# sourceMappingURL=version.js.map
