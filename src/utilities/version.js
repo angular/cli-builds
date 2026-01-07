ede90a2a3c23387404695fe83de3d94d9679623d
Angular Builds <dev-infra@angular.dev>
2026-01-07 19:10:42 +0000

    1ad7736 fix(@angular/cli): update dependency @modelcontextprotocol/sdk to v1.25.2

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-32adc3a');
+exports.VERSION = new Version('21.1.0-next.3+sha-1ad7736');
 //# sourceMappingURL=version.js.map
