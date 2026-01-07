a111c80ab64b658edb62c113ccfe9b592ad03da9
Angular Builds <dev-infra@angular.dev>
2026-01-07 17:31:01 +0000

    32adc3a fix(@angular/build): ensure correct project targeting during Vitest debugging

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-31132bc');
+exports.VERSION = new Version('21.1.0-next.3+sha-32adc3a');
 //# sourceMappingURL=version.js.map
