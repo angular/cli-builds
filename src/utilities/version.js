75afff0cf64323a734fa9c53b4d2141158e6b988
Angular Builds <dev-infra@angular.dev>
2026-01-07 00:32:03 +0000

    1e39c77 fix(@angular/build): inject source-map-support for Vitest browser tests

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.0-next.3+sha-bc48163');
+exports.VERSION = new Version('21.1.0-next.3+sha-1e39c77');
 //# sourceMappingURL=version.js.map
