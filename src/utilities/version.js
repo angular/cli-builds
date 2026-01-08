e8d49743d55bc20b629d2b2f5f757e8fa965aef4
Angular Builds <dev-infra@angular.dev>
2026-01-08 21:37:40 +0000

    27c5508 build: update all non-major dependencies

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-next.0+sha-937c71b');
+exports.VERSION = new Version('21.2.0-next.0+sha-27c5508');
 //# sourceMappingURL=version.js.map
