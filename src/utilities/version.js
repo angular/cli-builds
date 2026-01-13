f54ee67a83ba9ede4b5c06c62bdd863cd4200664
Angular Builds <dev-infra@angular.dev>
2026-01-13 18:08:27 +0000

    730ae66 fix(@angular/ssr): handle platform destruction during rendering

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.0.5+sha-6e496cc');
+exports.VERSION = new Version('21.0.5+sha-730ae66');
 //# sourceMappingURL=version.js.map
