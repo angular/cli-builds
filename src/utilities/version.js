b9efdb251d2c20d4e084d3fc05d1fa2150ff608e
Angular Builds <dev-infra@angular.dev>
2026-01-13 18:12:03 +0000

    79b69f5 build: lock file maintenance

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-next.0+sha-e6b62a1');
+exports.VERSION = new Version('21.2.0-next.0+sha-79b69f5');
 //# sourceMappingURL=version.js.map
