f8c2fa9fe66e2c63fc980dab6f18a25e923e6ebf
Angular Builds <dev-infra@angular.dev>
2026-01-26 09:31:32 +0000

    2d30639 fix(@angular/build): support merging coverage thresholds with Vitest runnerConfig

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.1.1+sha-165e7d6');
+exports.VERSION = new Version('21.1.1+sha-2d30639');
 //# sourceMappingURL=version.js.map
