c18ef190cee543e9618cb60bc1e06787a1b58a8e
Angular Builds <dev-infra@angular.dev>
2026-01-27 07:31:45 +0000

    a1249d1 ci: update Windows CI jobs to use Node.js 24 for test execution

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-next.0+sha-98a24d0');
+exports.VERSION = new Version('21.2.0-next.0+sha-a1249d1');
 //# sourceMappingURL=version.js.map
