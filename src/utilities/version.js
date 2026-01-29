66f70e1f2e800cefdc308bdbfe0e92d2a9fa2310
Angular Builds <dev-infra@angular.dev>
2026-01-29 14:30:07 +0000

    151b58a build: update actions/cache action to v5.0.3

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-next.0+sha-e5d3b94');
+exports.VERSION = new Version('21.2.0-next.0+sha-151b58a');
 //# sourceMappingURL=version.js.map
