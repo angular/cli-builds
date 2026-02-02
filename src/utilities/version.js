c9d8452b8523d2705a1a29c27cfd6d4abf6edfe2
Angular Builds <dev-infra@angular.dev>
2026-02-02 12:46:40 +0000

    5d1df50 fix(@schematics/angular): add actionable feedback to vitest-browser schematic

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-next.0+sha-51fc778');
+exports.VERSION = new Version('21.2.0-next.0+sha-5d1df50');
 //# sourceMappingURL=version.js.map
