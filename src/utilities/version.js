6b9c0e4c0f01f9f5bd896fc4271d4e7efbbdbbe5
Angular Builds <dev-infra@angular.dev>
2026-02-04 14:29:10 +0000

    346973a build: configure Jasmine to allow duplicate describe block names

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-next.0+sha-bc363af');
+exports.VERSION = new Version('21.2.0-next.0+sha-346973a');
 //# sourceMappingURL=version.js.map
