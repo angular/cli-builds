c5df2643d2db8349b1158d90b03d51d4c580fc40
Angular Builds <dev-infra@angular.dev>
2026-02-10 13:48:17 +0000

    a391f04 test: execute 'protractor' binary directly in 'browsers' E2E test

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-next.1+sha-826a994');
+exports.VERSION = new Version('21.2.0-next.1+sha-a391f04');
 //# sourceMappingURL=version.js.map
