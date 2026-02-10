94af4d45ac872c85299725805fe0e6442a8620f6
Angular Builds <dev-infra@angular.dev>
2026-02-10 12:35:54 +0000

    826a994 test: replace 'private-e2e' schematic with explicit setup in 'browsers' E2E test

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-next.1+sha-0e6f853');
+exports.VERSION = new Version('21.2.0-next.1+sha-826a994');
 //# sourceMappingURL=version.js.map
