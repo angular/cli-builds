b3aa2cabc3305650946efd74b0083c01b5b08dd9
Angular Builds <dev-infra@angular.dev>
2026-02-20 17:28:52 +0000

    9c2ddfe release: switch the next branch to v22.0.0-next.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.3.0-next.0+sha-9f8f40e');
+exports.VERSION = new Version('22.0.0-next.0+sha-9c2ddfe');
 //# sourceMappingURL=version.js.map
