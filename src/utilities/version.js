4f2891b450d4d0de346458d49a0747492add713e
Angular Builds <dev-infra@angular.dev>
2026-02-23 13:37:28 +0000

    662eca3 refactor(@angular/cli): use a regex to identify internal Angular framework versions for compatibility checks

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-f086ecc');
+exports.VERSION = new Version('22.0.0-next.0+sha-662eca3');
 //# sourceMappingURL=version.js.map
