97e63555c5280e6a6cefe4595b234f2bb08b2708
Angular Builds <dev-infra@angular.dev>
2026-02-23 17:23:54 +0000

    ec65a67 refactor(@angular/build): expose dev-server options normalization function

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-rc.0+sha-44da09c');
+exports.VERSION = new Version('21.2.0-rc.0+sha-ec65a67');
 //# sourceMappingURL=version.js.map
