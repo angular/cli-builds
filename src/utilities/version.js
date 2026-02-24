ae2ad618e44eeb80c78f033837f92e44c5785195
Angular Builds <dev-infra@angular.dev>
2026-02-24 09:21:28 +0000

    7ac80a8 fix(@angular/cli): simplify Angular version compatibility checks and add special handling for local builds of new major versions

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-9850ebc');
+exports.VERSION = new Version('22.0.0-next.0+sha-7ac80a8');
 //# sourceMappingURL=version.js.map
