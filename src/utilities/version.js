66033c6314f285ba796cd8cdb088bbcd0a02a0b1
Angular Builds <dev-infra@angular.dev>
2026-02-24 14:32:44 +0000

    25b8a15 fix(@angular/cli): quote complex range specifiers in package manager

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0-rc.2+sha-42f7d99');
+exports.VERSION = new Version('21.2.0-rc.2+sha-25b8a15');
 //# sourceMappingURL=version.js.map
