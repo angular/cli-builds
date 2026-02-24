d69a21bca7f87fabd82744b2ddf5de697b673a8a
Angular Builds <dev-infra@angular.dev>
2026-02-24 21:10:38 +0000

    dfbbe2d build: update dependency @listr2/prompt-adapter-inquirer to v4

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-1d35e3f');
+exports.VERSION = new Version('22.0.0-next.0+sha-dfbbe2d');
 //# sourceMappingURL=version.js.map
