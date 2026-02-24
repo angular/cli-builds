5193975fd304dfa8b66faa0c09781795d2325e0c
Angular Builds <dev-infra@angular.dev>
2026-02-24 21:59:27 +0000

    6584f5f refactor(@angular/build): remove Node.js v20 conditional file copying logic

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-dfbbe2d');
+exports.VERSION = new Version('22.0.0-next.0+sha-6584f5f');
 //# sourceMappingURL=version.js.map
