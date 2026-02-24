252d169e744b21af025baa9903ce99f7806327bc
Angular Builds <dev-infra@angular.dev>
2026-02-24 19:19:07 +0000

    1d35e3f build: update dependency @inquirer/confirm to v6

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-d0e9e81');
+exports.VERSION = new Version('22.0.0-next.0+sha-1d35e3f');
 //# sourceMappingURL=version.js.map
