d49339201d73d704bfb7a845bfd31dd01653dae8
Angular Builds <dev-infra@angular.dev>
2026-02-25 15:27:45 +0000

    b4885b8 refactor: remove experimental Jest and Web Test Runner builders

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-bfdc7b0');
+exports.VERSION = new Version('22.0.0-next.0+sha-b4885b8');
 //# sourceMappingURL=version.js.map
