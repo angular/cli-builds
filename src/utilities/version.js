37e7bd7b73290dc035f85bdb5534980c4450590a
Angular Builds <dev-infra@angular.dev>
2026-02-26 15:57:02 +0000

    15794dc refactor: centralize ESLint file targeting within `eslint.config.mjs` to process only TypeScript files and ignore JavaScript files

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-9d167ca');
+exports.VERSION = new Version('22.0.0-next.0+sha-15794dc');
 //# sourceMappingURL=version.js.map
