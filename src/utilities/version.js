50b201d71149c88670bbd47cb1dd39f10f33e70d
Angular Builds <dev-infra@angular.dev>
2026-02-26 14:29:52 +0000

    6699cdc perf(@angular/build): fix memory leak in `ng serve` with i18n

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0+sha-536a959');
+exports.VERSION = new Version('21.2.0+sha-6699cdc');
 //# sourceMappingURL=version.js.map
