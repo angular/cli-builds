7c476ee009d5479d1db3f78707d5f15799ccb924
Angular Builds <dev-infra@angular.dev>
2026-02-26 12:22:07 +0000

    536a959 build: update rules_browsers digest to 8231142

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0+sha-018d493');
+exports.VERSION = new Version('21.2.0+sha-536a959');
 //# sourceMappingURL=version.js.map
