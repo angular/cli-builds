15cbba8662810e9ff5b192c09c484481f43b89f1
Angular Builds <dev-infra@angular.dev>
2026-02-27 08:33:23 +0000

    27cd355 refactor(@angular/ssr): remove CSR fallback for invalid hosts

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-b5868a8');
+exports.VERSION = new Version('22.0.0-next.0+sha-27cd355');
 //# sourceMappingURL=version.js.map
