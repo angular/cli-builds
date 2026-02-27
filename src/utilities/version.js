4cb3c1c149b885e02eeb07c66b13ad875a3fcaaa
Angular Builds <dev-infra@angular.dev>
2026-02-27 15:14:33 +0000

    e0fa7bf refactor: remove `tree-kill` dependency by using native `child_process.kill` and correcting `spawn` usage

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-0a61f20');
+exports.VERSION = new Version('22.0.0-next.0+sha-e0fa7bf');
 //# sourceMappingURL=version.js.map
