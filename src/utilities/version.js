66675e6f6e70fcfa91750ce2944bc3b4c39cda7b
Angular Builds <dev-infra@angular.dev>
2026-02-27 09:51:24 +0000

    01f4fc0 build: update repository locations for bazel rules

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-27cd355');
+exports.VERSION = new Version('22.0.0-next.0+sha-01f4fc0');
 //# sourceMappingURL=version.js.map
