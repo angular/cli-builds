15f7cb3e000cdd1b920f015f0955b725a3c87d0b
Angular Builds <dev-infra@angular.dev>
2026-02-28 02:05:23 +0000

    b697145 build: update dependency bazel to v8.6.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-6a1471b');
+exports.VERSION = new Version('22.0.0-next.0+sha-b697145');
 //# sourceMappingURL=version.js.map
