dd8b6040a1bcd5f15ed6adf4fd2a4e0f4b626dde
Angular Builds <dev-infra@angular.dev>
2026-03-02 22:44:49 +0000

    b47078f refactor(@ngtools/webpack): support optional baseUrl with tsconfig paths

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-73b9e37');
+exports.VERSION = new Version('22.0.0-next.0+sha-b47078f');
 //# sourceMappingURL=version.js.map
