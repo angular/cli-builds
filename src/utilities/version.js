656f25662b2be5b4bf65a1cb7d5ab60b908cc43d
Angular Builds <dev-infra@angular.dev>
2026-03-02 16:23:51 +0000

    73b9e37 Revert "refactor: remove `tree-kill` dependency and refactor `killAllProcesses` to use native `childProc.kill` with retries."

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-414320d');
+exports.VERSION = new Version('22.0.0-next.0+sha-73b9e37');
 //# sourceMappingURL=version.js.map
