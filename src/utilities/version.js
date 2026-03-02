baffd2480833b731255c26efafeb25c760da81a2
Angular Builds <dev-infra@angular.dev>
2026-03-02 14:06:47 +0000

    6ad8608 fix(@angular/build): bundle polyfills to preserve execution order in dev server

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0+sha-af70c46');
+exports.VERSION = new Version('21.2.0+sha-6ad8608');
 //# sourceMappingURL=version.js.map
