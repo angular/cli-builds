64ca47f2434f2022fa550600bde95c4b66884c5e
Angular Builds <dev-infra@angular.dev>
2026-03-03 15:27:53 +0000

    0f41585 fix(@angular/ssr): introduce DI token to signal route discovery process

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-47dae10');
+exports.VERSION = new Version('22.0.0-next.0+sha-0f41585');
 //# sourceMappingURL=version.js.map
