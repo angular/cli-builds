621480f4faccfab77cf932aa75805aa118f470d6
Angular Builds <dev-infra@angular.dev>
2026-03-03 21:47:35 +0000

    62a2874 ci: delete feature request workflow

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-8350e42');
+exports.VERSION = new Version('22.0.0-next.0+sha-62a2874');
 //# sourceMappingURL=version.js.map
