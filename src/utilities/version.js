e40cf64f2fa4d65eed2efd351079ef55fe6273a6
Angular Builds <dev-infra@angular.dev>
2026-03-03 17:25:45 +0000

    4dc8ea0 build: update dependency verdaccio to v6.2.9

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.0+sha-fd5dcf3');
+exports.VERSION = new Version('21.2.0+sha-4dc8ea0');
 //# sourceMappingURL=version.js.map
