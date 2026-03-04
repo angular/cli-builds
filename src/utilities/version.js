fa1d01e960ecf9ed571018227cb369686e7ef16e
Angular Builds <dev-infra@angular.dev>
2026-03-04 12:37:39 +0000

    108fe07 fix(@angular/build): conditionally allow `vi.mock` for non-relative imports

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-6340666');
+exports.VERSION = new Version('22.0.0-next.0+sha-108fe07');
 //# sourceMappingURL=version.js.map
