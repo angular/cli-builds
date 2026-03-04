1eb84a5bfd19cf9cc1b4c3d4fde80012069dc873
Angular Builds <dev-infra@angular.dev>
2026-03-04 09:40:03 +0000

    843ca01 ci: update pull-request labeling action path

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-6293cae');
+exports.VERSION = new Version('22.0.0-next.0+sha-843ca01');
 //# sourceMappingURL=version.js.map
