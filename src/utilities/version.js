cb6da1a2935b236da87d1b8b68ea73440273dc9a
Angular Builds <dev-infra@angular.dev>
2026-03-04 13:38:02 +0000

    ac065ad fix(@angular/build): resolve style include paths relative to `ng-package.json` in unit-test builder

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-1d7fceb');
+exports.VERSION = new Version('22.0.0-next.0+sha-ac065ad');
 //# sourceMappingURL=version.js.map
