caedcba308d8f2747c79f9a0f8eeb873447a9433
Angular Builds <dev-infra@angular.dev>
2026-03-04 02:30:07 +0000

    f4c9f2f build: update dependency copy-webpack-plugin to v14

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-62a2874');
+exports.VERSION = new Version('22.0.0-next.0+sha-f4c9f2f');
 //# sourceMappingURL=version.js.map
