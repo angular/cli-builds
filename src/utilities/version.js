fa4189da9201144a7ae7d6d4443d3969e34522ba
Angular Builds <dev-infra@angular.dev>
2026-03-04 13:18:47 +0000

    1d7fceb build: update to TypeScript 6

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-108fe07');
+exports.VERSION = new Version('22.0.0-next.0+sha-1d7fceb');
 //# sourceMappingURL=version.js.map
