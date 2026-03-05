16fae3afb2723096d1dbe50d1c65f054118a78fc
Angular Builds <dev-infra@angular.dev>
2026-03-05 15:03:37 +0000

    4d345b5 ci: remove redundant `actions/checkout` steps from dev-infra workflows

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-4e8ade8');
+exports.VERSION = new Version('22.0.0-next.0+sha-4d345b5');
 //# sourceMappingURL=version.js.map
