447066c60e300d42a5153e1ade885f02d0c2a219
Angular Builds <dev-infra@angular.dev>
2026-03-06 15:35:48 +0000

    d2743e2 build: update all non-major dependencies

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-93b1deb');
+exports.VERSION = new Version('22.0.0-next.0+sha-d2743e2');
 //# sourceMappingURL=version.js.map
