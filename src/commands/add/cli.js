12b014327b9e840c42c63890db6049ef682d8884
Angular Builds <dev-infra@angular.dev>
2026-03-09 15:42:42 +0000

    d2f2098 fix(@angular/cli): preserve exact version in ng add when requested

--- src/commands/add/cli.js
+++ src/commands/add/cli.js
@@ -164,6 +164,7 @@ class AddCommandModule extends schematics_command_module_1.SchematicsCommandModu
         }
         const taskContext = {
             packageIdentifier,
+            isExactVersion: packageIdentifier.type === 'version',
             executeSchematic: this.executeSchematic.bind(this),
             getPeerDependencyConflicts: this.getPeerDependencyConflicts.bind(this),
             dryRun: options.dryRun,
@@ -419,7 +420,7 @@ class AddCommandModule extends schematics_command_module_1.SchematicsCommandModu
             context.packageIdentifier = npm_package_arg_1.default.resolve(context.packageIdentifier.name, 
             // `save-prefix` option is ignored by some package managers so the caret is needed to ensure
             // that the value in the project package.json is correct.
-            '^' + manifest.version);
+            (context.isExactVersion ? '' : '^') + manifest.version);
         }
         context.hasSchematics = !!manifest.schematics;
         context.savePackage = manifest['ng-add']?.save;
--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.1+sha-3af71f9');
+exports.VERSION = new Version('21.2.1+sha-d2f2098');
 //# sourceMappingURL=version.js.map
