287e5be2a07c5a6968b39777156a7eafa1b893bf
Angular Builds <dev-infra@angular.dev>
2026-03-09 15:15:12 +0000

    6e4aa3b build: fix broken lock file

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.0+sha-832bb49');
+exports.VERSION = new Version('22.0.0-next.0+sha-6e4aa3b');
 //# sourceMappingURL=version.js.map
