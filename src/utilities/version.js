247f6bf8414a7b516d378709bcce318abb82ab11
Angular Builds <dev-infra@angular.dev>
2026-03-12 10:23:15 +0000

    cb57740 build: update actions/download-artifact action to v8.0.1

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.1+sha-6572a69');
+exports.VERSION = new Version('22.0.0-next.1+sha-cb57740');
 //# sourceMappingURL=version.js.map
