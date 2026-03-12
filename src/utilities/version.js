7006dd2152a26c55f3e59bee31aae5837c63d0e1
Angular Builds <dev-infra@angular.dev>
2026-03-12 09:29:31 +0000

    6572a69 fix(@schematics/angular): default components to OnPush change detection

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.1+sha-174c786');
+exports.VERSION = new Version('22.0.0-next.1+sha-6572a69');
 //# sourceMappingURL=version.js.map
