7cd55bb9aa367da7fd16a026748352b44acbe3ed
Angular Builds <dev-infra@angular.dev>
2026-03-17 17:52:57 +0000

    b8bcd59 fix(@angular/ssr): ensure unique values in redirect response Vary header

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.2+sha-1505164');
+exports.VERSION = new Version('21.2.2+sha-b8bcd59');
 //# sourceMappingURL=version.js.map
