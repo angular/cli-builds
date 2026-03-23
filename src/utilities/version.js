fb1b9a59d06ec950114015cda323c8aa60b61516
Angular Builds <dev-infra@angular.dev>
2026-03-23 16:15:21 +0000

    240870e test: ignore HMR disabled console errors

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.2+sha-742a874');
+exports.VERSION = new Version('22.0.0-next.2+sha-240870e');
 //# sourceMappingURL=version.js.map
