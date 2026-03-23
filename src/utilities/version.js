72243909fb161af1581b1bb7a90756bae3c8b680
Angular Builds <dev-infra@angular.dev>
2026-03-23 13:43:59 +0000

    742a874 ci: block esbuild-wasm updates due to a regression

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.2+sha-65f39d0');
+exports.VERSION = new Version('22.0.0-next.2+sha-742a874');
 //# sourceMappingURL=version.js.map
