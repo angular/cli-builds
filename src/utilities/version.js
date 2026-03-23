36fad7332908f301083ccf33d401756a71d74492
Angular Builds <dev-infra@angular.dev>
2026-03-23 11:36:56 +0000

    355ebe8 fix(@angular/build): prevent reporter duplicates by explicitly overriding Vitest configuration

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.2+sha-1a8376b');
+exports.VERSION = new Version('22.0.0-next.2+sha-355ebe8');
 //# sourceMappingURL=version.js.map
