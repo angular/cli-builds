987607ede3a8dfa1a9248d53e0d2fe5bd1f3eb04
Angular Builds <dev-infra@angular.dev>
2026-03-27 08:58:46 +0000

    f1ed025 refactor(@angular-devkit/schematics): remove shell usage in git spawn to prevent command injection

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.3+sha-a3f629a');
+exports.VERSION = new Version('22.0.0-next.3+sha-f1ed025');
 //# sourceMappingURL=version.js.map
