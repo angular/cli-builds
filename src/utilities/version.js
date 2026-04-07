c7af2cbf96c7c5ade575a2c006f772b4603a07d3
Angular Builds <dev-infra@angular.dev>
2026-04-07 13:29:33 +0000

    422c818 fix(@angular/cli): handle missing package manager during analytics initialization

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.4+sha-617dd4e');
+exports.VERSION = new Version('22.0.0-next.4+sha-422c818');
 //# sourceMappingURL=version.js.map
