d25453d099449c2f7544e9887d4eea541ddd0a87
Angular Builds <dev-infra@angular.dev>
2026-04-10 13:47:45 +0000

    268e159 build: move TypeScript and Angular Compiler CLI to dependencies

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.7+sha-22a108e');
+exports.VERSION = new Version('21.2.7+sha-268e159');
 //# sourceMappingURL=version.js.map
