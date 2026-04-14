16c9d3a555b3d3116cc5b3bb69a5c1162843f921
Angular Builds <dev-infra@angular.dev>
2026-04-14 15:24:20 +0000

    de630c2 feat(@schematics/angular): stabilize refactor-jasmine-vitest schematic

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.5+sha-777dfdd');
+exports.VERSION = new Version('22.0.0-next.5+sha-de630c2');
 //# sourceMappingURL=version.js.map
