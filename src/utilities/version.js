dea06cb54cc9156deedc2f449b2e0a84dbb2af82
Angular Builds <dev-infra@angular.dev>
2026-04-17 14:04:42 +0000

    afb4ef2 build: update schematics dependencies to ~6.2.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.5+sha-cf5b49e');
+exports.VERSION = new Version('22.0.0-next.5+sha-afb4ef2');
 //# sourceMappingURL=version.js.map
