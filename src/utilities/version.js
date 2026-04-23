9bbf63e577d74b73d6f5d3389511f6c27308495b
Angular Builds <dev-infra@angular.dev>
2026-04-23 09:02:04 +0000

    73233dc feat(@angular/build): support Istanbul coverage in Vitest runner

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.6+sha-0122398');
+exports.VERSION = new Version('22.0.0-next.6+sha-73233dc');
 //# sourceMappingURL=version.js.map
