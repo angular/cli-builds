9403313f67a89537a20e188726ab6205419c96fc
Angular Builds <dev-infra@angular.dev>
2026-04-24 08:05:35 +0000

    2932ff0 build: update dependency verdaccio-auth-memory to v13

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.6+sha-73233dc');
+exports.VERSION = new Version('22.0.0-next.6+sha-2932ff0');
 //# sourceMappingURL=version.js.map
