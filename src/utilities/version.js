021345511b16600f4e194b7d917fe42f67bc8b29
Angular Builds <dev-infra@angular.dev>
2026-04-30 08:10:05 +0000

    0841637 test: allow underscores in hash for server routes preload links

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-b5ba365');
+exports.VERSION = new Version('22.0.0-next.7+sha-0841637');
 //# sourceMappingURL=version.js.map
