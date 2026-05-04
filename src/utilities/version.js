4d0591177b0f27b97e84d79904ddbe7540ebee08
Angular Builds <dev-infra@angular.dev>
2026-05-04 17:34:38 +0000

    d227e69 feat(@schematics/angular): migrate fake async to Vitest fake timers

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-7fb59ea');
+exports.VERSION = new Version('22.0.0-next.7+sha-d227e69');
 //# sourceMappingURL=version.js.map
