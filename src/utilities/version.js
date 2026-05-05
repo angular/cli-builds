715d440396c984ab95273d62845691768b4a4070
Angular Builds <dev-infra@angular.dev>
2026-05-05 00:08:28 +0000

    deca40b build: update all non-major dependencies

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-086e081');
+exports.VERSION = new Version('22.0.0-next.7+sha-deca40b');
 //# sourceMappingURL=version.js.map
