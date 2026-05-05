2d39953d0dee47d4435c34a4e7de7cd72db08e40
Angular Builds <dev-infra@angular.dev>
2026-05-05 21:01:17 +0000

    58c7c7a feat(@angular/build): subresource integrity validation for dynamically loaded modules

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-c9f4081');
+exports.VERSION = new Version('22.0.0-next.7+sha-58c7c7a');
 //# sourceMappingURL=version.js.map
