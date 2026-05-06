5e4ba265642c8bddef9d2c4bd5088573411ebefe
Angular Builds <dev-infra@angular.dev>
2026-05-06 16:58:39 +0000

    23af499 test: mark deprecated public APIs in build_webpack, ngtools/webpack, and ssr/node packages

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-bf94764');
+exports.VERSION = new Version('22.0.0-next.7+sha-23af499');
 //# sourceMappingURL=version.js.map
