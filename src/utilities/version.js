86725607e03de62b04e13353f64daf5a7cf8ca7c
Angular Builds <dev-infra@angular.dev>
2026-05-06 13:23:27 +0000

    58c0978 feat(@angular/cli): add support for Node.js 26.0.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-58c7c7a');
+exports.VERSION = new Version('22.0.0-next.7+sha-58c0978');
 //# sourceMappingURL=version.js.map
