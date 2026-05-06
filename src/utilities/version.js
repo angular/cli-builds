a98c9d836332a50563acd9264fe0925027c7ceb1
Angular Builds <dev-infra@angular.dev>
2026-05-06 16:46:23 +0000

    bf94764 build: remove rootDir from root configuration

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-a5e1e48');
+exports.VERSION = new Version('22.0.0-next.7+sha-bf94764');
 //# sourceMappingURL=version.js.map
