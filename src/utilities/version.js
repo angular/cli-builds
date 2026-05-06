513be946eeb63da9f1d807eccc7b800ea0ce9a60
Angular Builds <dev-infra@angular.dev>
2026-05-06 20:01:06 +0000

    d2aa9ed feat(@schematics/angular): migrate fakeAsync's flush behavior when used in beforeEach

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-23af499');
+exports.VERSION = new Version('22.0.0-next.7+sha-d2aa9ed');
 //# sourceMappingURL=version.js.map
