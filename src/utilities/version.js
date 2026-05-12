1cc4f2dbb1e1efe8b01dd3d8eadf8331e5b90792
Angular Builds <dev-infra@angular.dev>
2026-05-12 14:39:16 +0000

    653e4e9 refactor(@angular-devkit/schematics-cli): use custom indent logger and improve tests

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.8+sha-3ab0152');
+exports.VERSION = new Version('22.0.0-next.8+sha-653e4e9');
 //# sourceMappingURL=version.js.map
