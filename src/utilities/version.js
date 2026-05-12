01648749359077d9aeb897cb8174515899ac9994
Angular Builds <dev-infra@angular.dev>
2026-05-12 18:31:16 +0000

    71f0b0a ci: update Renovate base branch patterns to 22.0.x

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.8+sha-653e4e9');
+exports.VERSION = new Version('22.1.0-next.0+sha-71f0b0a');
 //# sourceMappingURL=version.js.map
