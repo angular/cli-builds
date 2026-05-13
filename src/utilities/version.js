415c43bc4efe9aaab81c2c4d1581427c540b9551
Angular Builds <dev-infra@angular.dev>
2026-05-13 08:33:32 +0000

    6131ea1 refactor(@angular-devkit/core): deprecate createConsoleLogger

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-ea95e1a');
+exports.VERSION = new Version('22.0.0-next.7+sha-6131ea1');
 //# sourceMappingURL=version.js.map
