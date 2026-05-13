b05ce6792deaaca2bbd22ada858edbdb2051c2e2
Angular Builds <dev-infra@angular.dev>
2026-05-13 14:55:59 +0000

    c14eeca refactor(@angular/build): remove unused target parameter from getFeatureSupport

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-b75423d');
+exports.VERSION = new Version('22.1.0-next.0+sha-c14eeca');
 //# sourceMappingURL=version.js.map
