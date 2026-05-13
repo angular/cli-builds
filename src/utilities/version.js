33c394d438b7919609c0cfadd90f83c1a40d6dda
Angular Builds <dev-infra@angular.dev>
2026-05-13 10:39:31 +0000

    f853439 fix(@angular/ssr): support all X-Forwarded-* headers when trustProxyHeaders is true

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-next.7+sha-6131ea1');
+exports.VERSION = new Version('22.0.0-next.7+sha-f853439');
 //# sourceMappingURL=version.js.map
