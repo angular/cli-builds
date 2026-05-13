43ef9222d6cf29cf1f1a5d1bfda474c205683f3e
Angular Builds <dev-infra@angular.dev>
2026-05-13 19:18:23 +0000

    7e980ad docs: update ng.ts render function documentation to reflect removal of rendering implementation details

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-c14eeca');
+exports.VERSION = new Version('22.1.0-next.0+sha-7e980ad');
 //# sourceMappingURL=version.js.map
