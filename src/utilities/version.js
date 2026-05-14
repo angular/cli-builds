62f44cc15512e374914b5381238b242c6a29de17
Angular Builds <dev-infra@angular.dev>
2026-05-14 15:07:20 +0000

    3f4c79b docs: update JSDoc return tag to `@return` and fix description in registry.ts

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-rc.0+sha-99ed7a7');
+exports.VERSION = new Version('22.0.0-rc.0+sha-3f4c79b');
 //# sourceMappingURL=version.js.map
