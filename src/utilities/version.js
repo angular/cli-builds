aea9a21eb7e0997861d1c0101ad29eb8c5675ccd
Angular Builds <dev-infra@angular.dev>
2026-05-21 14:07:34 +0000

    163a032 fix(@angular/build): assert that asset input paths are within workspace root

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-981b882');
+exports.VERSION = new Version('22.1.0-next.0+sha-163a032');
 //# sourceMappingURL=version.js.map
