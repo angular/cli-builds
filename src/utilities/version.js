c6fbfe6de90ad4127b9668bb526fc80d2f75c003
Angular Builds <dev-infra@angular.dev>
2026-05-27 21:21:01 +0000

    8ae13a4 docs: release notes for the v21.2.13 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-e12299e');
+exports.VERSION = new Version('22.1.0-next.0+sha-8ae13a4');
 //# sourceMappingURL=version.js.map
