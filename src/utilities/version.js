9716af32ca03494fda48647494f1a950a8431b12
Angular Builds <dev-infra@angular.dev>
2026-05-29 11:18:28 +0000

    aed4487 fix(@angular/cli): expand package groups for newly added peer dependencies in update schematic

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.13+sha-287e4e8');
+exports.VERSION = new Version('21.2.13+sha-aed4487');
 //# sourceMappingURL=version.js.map
