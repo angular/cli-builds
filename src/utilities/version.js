67284e9c3c3e7f7e9f7bb1f759b6c58cd11d7ff9
Angular Builds <dev-infra@angular.dev>
2026-05-29 15:49:34 +0000

    dea19c8 build: update pnpm to v10.34.1

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0-rc.2+sha-418abd8');
+exports.VERSION = new Version('22.0.0-rc.2+sha-dea19c8');
 //# sourceMappingURL=version.js.map
