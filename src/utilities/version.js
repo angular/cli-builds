26a35a7cb025ae3f13b42ab80a2a557593667e76
Angular Builds <dev-infra@angular.dev>
2026-06-03 14:12:10 +0000

    09d3bbb docs: update changelog with v22 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-159d245');
+exports.VERSION = new Version('22.1.0-next.0+sha-09d3bbb');
 //# sourceMappingURL=version.js.map
