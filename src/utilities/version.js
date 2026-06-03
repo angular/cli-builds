b8bd99b613d60b27eb8a0ebe7de08545386a6312
Angular Builds <dev-infra@angular.dev>
2026-06-03 15:26:30 +0000

    11a4438 test: add 20.0-project E2E test asset and update update tests to use it

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-09d3bbb');
+exports.VERSION = new Version('22.1.0-next.0+sha-11a4438');
 //# sourceMappingURL=version.js.map
