251bde0d9809aa26d6b711b0bb2186fc7f19e5e5
Angular Builds <dev-infra@angular.dev>
2026-06-05 14:30:51 +0000

    bd413b0 fix(@angular/build): restrict application builder output paths to output directory

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.0+sha-c800122');
+exports.VERSION = new Version('22.0.0+sha-bd413b0');
 //# sourceMappingURL=version.js.map
