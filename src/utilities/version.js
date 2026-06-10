9a3deea637b895389185a43227d0dd44a3d0a079
Angular Builds <dev-infra@angular.dev>
2026-06-10 13:58:53 +0000

    f8c3df4 fix(@angular/cli): remove forceAuth and unscoped credential parsing

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-96202a3');
+exports.VERSION = new Version('22.1.0-next.0+sha-f8c3df4');
 //# sourceMappingURL=version.js.map
