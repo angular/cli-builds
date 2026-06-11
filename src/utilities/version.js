bc064c8379b20b75c13b3c9f8f723bc0a71a5211
Angular Builds <dev-infra@angular.dev>
2026-06-11 13:19:13 +0000

    0130da9 build: bump Angular framework and ng-packagr versions to 22.1.0-next.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-e8312fa');
+exports.VERSION = new Version('22.1.0-next.0+sha-0130da9');
 //# sourceMappingURL=version.js.map
