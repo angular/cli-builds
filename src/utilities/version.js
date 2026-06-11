224f54764e6d6df693848317d49db05273cbb265
Angular Builds <dev-infra@angular.dev>
2026-06-11 16:58:24 +0000

    c8088a5 fix(@angular/ssr): prioritize options over environment variables in AngularNodeAppEngine

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.1+sha-285a34e');
+exports.VERSION = new Version('22.0.1+sha-c8088a5');
 //# sourceMappingURL=version.js.map
