bd1609e9ceb0ad558baea973750f3005e5f5e7b4
Angular Builds <dev-infra@angular.dev>
2026-06-12 21:36:34 +0000

    0cebf18 build: update all github actions to v4.36.2

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.1+sha-c8088a5');
+exports.VERSION = new Version('22.0.1+sha-0cebf18');
 //# sourceMappingURL=version.js.map
