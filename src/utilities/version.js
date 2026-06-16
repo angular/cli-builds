21dc9c4805c57be78cdccbf9c29eca1d0afdddbb
Angular Builds <dev-infra@angular.dev>
2026-06-16 13:12:21 +0000

    d996a27 fix(@angular/ssr): avoid caching non-SSG page lookups

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.1+sha-2879ed9');
+exports.VERSION = new Version('22.0.1+sha-d996a27');
 //# sourceMappingURL=version.js.map
