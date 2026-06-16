b7dd882445eac8ac30433dc931bed04f81ef02c1
Angular Builds <dev-infra@angular.dev>
2026-06-16 07:26:15 +0000

    3ce6e5f perf(@angular/cli): implement semaphore backpressure throttling in PackageManager

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-583736a');
+exports.VERSION = new Version('22.1.0-next.0+sha-3ce6e5f');
 //# sourceMappingURL=version.js.map
