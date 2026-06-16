97df0891d3aa86739a732d7eda87070867920656
Angular Builds <dev-infra@angular.dev>
2026-06-16 09:15:25 +0000

    a527d02 refactor: move eslint disable comment to top of file

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-3ce6e5f');
+exports.VERSION = new Version('22.1.0-next.0+sha-a527d02');
 //# sourceMappingURL=version.js.map
