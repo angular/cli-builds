8695715b1c63da02c96fc3f4ef236eeb5445dadc
Angular Builds <dev-infra@angular.dev>
2026-06-17 23:18:36 +0000

    0eddea8 fix(@schematics/angular): remove default workspace vscode mcp.json configuration

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.2+sha-08f9959');
+exports.VERSION = new Version('22.0.2+sha-0eddea8');
 //# sourceMappingURL=version.js.map
