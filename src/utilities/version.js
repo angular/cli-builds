c865faf428b53a7a82e1c86fcbfc36020a5729e5
Angular Builds <dev-infra@angular.dev>
2026-06-22 14:18:38 +0000

    b9681c0 build: upgrade Vite to v8.0.16

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-87c8318');
+exports.VERSION = new Version('22.1.0-next.0+sha-b9681c0');
 //# sourceMappingURL=version.js.map
