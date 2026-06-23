a966e1e7fe37a5a5293cf7aea944a84cf92f2788
Angular Builds <dev-infra@angular.dev>
2026-06-23 08:53:55 +0000

    7ef9ed2 feat(@angular/ssr): support the standard Forwarded header

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.0+sha-316cc20');
+exports.VERSION = new Version('22.1.0-next.0+sha-7ef9ed2');
 //# sourceMappingURL=version.js.map
