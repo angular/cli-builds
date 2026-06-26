05689ed4846f78a578dfc5f5611be47676bd26c8
Angular Builds <dev-infra@angular.dev>
2026-06-26 12:58:28 +0000

    2510ee3 fix(@angular/build): bump vite to 7.3.6

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.17+sha-61a810a');
+exports.VERSION = new Version('21.2.17+sha-2510ee3');
 //# sourceMappingURL=version.js.map
