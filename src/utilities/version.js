339967411e7bb3c14207a3e6a6737c0e305a2928
Angular Builds <dev-infra@angular.dev>
2026-06-26 08:53:59 +0000

    010cef6 fix(@angular/cli): gracefully handle package manager errors in command handler

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.1+sha-edb4594');
+exports.VERSION = new Version('22.1.0-next.1+sha-010cef6');
 //# sourceMappingURL=version.js.map
