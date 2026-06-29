44518edffa347438f858780c6ffb81c5dc5062d5
Angular Builds <dev-infra@angular.dev>
2026-06-29 13:03:22 +0000

    2d3eb7f fix(@angular-devkit/build-angular): prevent OS command injection in ssr-dev-server builder

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.1+sha-ae0c03c');
+exports.VERSION = new Version('22.1.0-next.1+sha-2d3eb7f');
 //# sourceMappingURL=version.js.map
