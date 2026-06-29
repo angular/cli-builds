4a025b021d7b045caaa1ec37c5c02ef909520273
Angular Builds <dev-infra@angular.dev>
2026-06-29 14:11:13 +0000

    5f774f8 fix(@angular/ssr): preserve response headers during redirect

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.4+sha-7c0d9f0');
+exports.VERSION = new Version('22.0.4+sha-5f774f8');
 //# sourceMappingURL=version.js.map
