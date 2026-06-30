7bce57395dfab3f63930320c0a034903140ca088
Angular Builds <dev-infra@angular.dev>
2026-06-30 07:23:40 +0000

    0010b92 fix(@angular/build): scope createRequire module resolution using paths to prevent parent paths

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.1+sha-d9fb854');
+exports.VERSION = new Version('22.1.0-next.1+sha-0010b92');
 //# sourceMappingURL=version.js.map
