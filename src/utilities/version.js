682e4fa111d33d448bc124b7fb2fbe4fd2219dcf
Angular Builds <dev-infra@angular.dev>
2026-07-06 15:16:15 +0000

    92da0d6 refactor(@angular/build): add NG_BUILD_CACHE_STORE environment variable support

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.2+sha-131b4c9');
+exports.VERSION = new Version('22.1.0-next.2+sha-92da0d6');
 //# sourceMappingURL=version.js.map
