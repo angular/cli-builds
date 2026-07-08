4011c1009e73f74e0bf9c0d890a5de311db28559
Angular Builds <dev-infra@angular.dev>
2026-07-08 15:01:12 +0000

    de5094b build: update all github actions

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.2+sha-41bd100');
+exports.VERSION = new Version('22.1.0-next.2+sha-de5094b');
 //# sourceMappingURL=version.js.map
