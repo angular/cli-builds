4dc08bdbbd848adc7fdfa1fc7458a7fd881282f5
Angular Builds <dev-infra@angular.dev>
2026-07-08 14:38:56 +0000

    df49966 fix(@angular/build): fallback to build target preserveSymlinks option in karma runner

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.5+sha-c9aa354');
+exports.VERSION = new Version('22.0.5+sha-df49966');
 //# sourceMappingURL=version.js.map
