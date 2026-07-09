d0cd7d669a0a422236c7f89e0474913bc8952128
Angular Builds <dev-infra@angular.dev>
2026-07-09 14:31:30 +0000

    cc45a54 build: update dependency http-proxy-middleware to v3.0.7

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.6+sha-31345b6');
+exports.VERSION = new Version('22.0.6+sha-cc45a54');
 //# sourceMappingURL=version.js.map
