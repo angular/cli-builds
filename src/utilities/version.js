97598f9c1bab7b98cd8e1ca2bcf800e01307edad
Angular Builds <dev-infra@angular.dev>
2026-07-10 20:23:52 +0000

    e5fff87 test(@angular/cli): verify package group locking under peer updates

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.3+sha-137d924');
+exports.VERSION = new Version('22.1.0-next.3+sha-e5fff87');
 //# sourceMappingURL=version.js.map
