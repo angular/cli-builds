75672cbdd0c44fae6ee7d153cb52143dc0b46aa0
Angular Builds <dev-infra@angular.dev>
2026-07-16 19:49:16 +0000

    d79c10a build: update dependency vite to v7.3.6

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.7+sha-1e556b3');
+exports.VERSION = new Version('22.0.7+sha-d79c10a');
 //# sourceMappingURL=version.js.map
