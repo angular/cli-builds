16376f9ea2f807a15261e5da3837e6a53e02145a
Angular Builds <dev-infra@angular.dev>
2026-07-17 18:14:44 +0000

    e4cbf33 build: update dependency quicktype-core to v25

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.4+sha-a2346bf');
+exports.VERSION = new Version('22.1.0-next.4+sha-e4cbf33');
 //# sourceMappingURL=version.js.map
