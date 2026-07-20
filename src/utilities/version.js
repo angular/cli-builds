356bd145b9254aea223650ca3c029b568e000c72
Angular Builds <dev-infra@angular.dev>
2026-07-20 15:46:02 +0000

    4187e5e fix(@angular/build): canonicalize drive letter casing for workspace root on Windows

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.4+sha-dabd1c0');
+exports.VERSION = new Version('22.1.0-next.4+sha-4187e5e');
 //# sourceMappingURL=version.js.map
