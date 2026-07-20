5e75905589efc55045f540bd8e9f7b2623db32cb
Angular Builds <dev-infra@angular.dev>
2026-07-20 14:58:26 +0000

    dabd1c0 build: update dependency magic-string to v1

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.4+sha-8f447ef');
+exports.VERSION = new Version('22.1.0-next.4+sha-dabd1c0');
 //# sourceMappingURL=version.js.map
