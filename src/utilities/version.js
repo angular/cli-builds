bdac4ab354735af48d6810c12f912dbc0c80d57f
Angular Builds <dev-infra@angular.dev>
2026-07-21 12:20:14 +0000

    54ae2a7 build: update devinfra commit and adjust bazel ts version validation dependency

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-next.4+sha-0947f81');
+exports.VERSION = new Version('22.1.0-next.4+sha-54ae2a7');
 //# sourceMappingURL=version.js.map
