6800b1139754ab4d38ebc14447cdd05e6b79862b
Angular Builds <dev-infra@angular.dev>
2026-07-23 14:09:58 +0000

    bc87c85 fix(@angular/cli): update MCP devserver tool names to conform to spec regex

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.7+sha-212a7bb');
+exports.VERSION = new Version('22.0.7+sha-bc87c85');
 //# sourceMappingURL=version.js.map
