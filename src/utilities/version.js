e61e37f09232ec5bf2f1419bf70b01325e3878e8
Angular Builds <dev-infra@angular.dev>
2026-07-23 15:40:19 +0000

    1bb2cc5 build: update devinfra digest to 8819f0b

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.7+sha-bc87c85');
+exports.VERSION = new Version('22.0.7+sha-1bb2cc5');
 //# sourceMappingURL=version.js.map
