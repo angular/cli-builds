9f7e2c9f292ff4db86d43404ba1582dd83331743
Angular Builds <dev-infra@angular.dev>
2026-07-27 07:08:20 +0000

    f8ccc41 build: update dependency listr2 to v11

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.0+sha-5a2f662');
+exports.VERSION = new Version('22.2.0-next.0+sha-f8ccc41');
 //# sourceMappingURL=version.js.map
