23d729edd7512d135405568e6f5cd97d35825770
Angular Builds <dev-infra@angular.dev>
2026-07-28 08:09:47 +0000

    d0c776d fix(@angular/build): safeguard Karma builder stream controller against closed state

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.0.8+sha-beac493');
+exports.VERSION = new Version('22.0.8+sha-d0c776d');
 //# sourceMappingURL=version.js.map
