74f532a3ae1158315716e8523b1c8c000664374a
Angular Builds <dev-infra@angular.dev>
2026-07-29 07:08:26 +0000

    2167cfc build: update dependency jsdom to v30

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.0+sha-fb997ce');
+exports.VERSION = new Version('22.2.0-next.0+sha-2167cfc');
 //# sourceMappingURL=version.js.map
