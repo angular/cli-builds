bdbcc5b597d02ba8381225d49c474580c6898c78
Angular Builds <dev-infra@angular.dev>
2026-07-29 21:32:21 +0000

    975554c build: update Angular framework and ng-packagr versions to 22.1.0 stable release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.0-rc.0+sha-96ea55a');
+exports.VERSION = new Version('22.1.0-rc.0+sha-975554c');
 //# sourceMappingURL=version.js.map
