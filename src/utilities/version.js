198aa8cb0f8431da868ba7eaa3133bc8b71cbf55
Angular Builds <dev-infra@angular.dev>
2026-07-30 14:35:21 +0000

    93cd752 ci: add branch tracking comments to dev-infra GitHub actions

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.0+sha-c43653e');
+exports.VERSION = new Version('22.2.0-next.0+sha-93cd752');
 //# sourceMappingURL=version.js.map
