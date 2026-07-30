9350f260940a29f013d123ea3c23bf6af31ee4aa
Angular Builds <dev-infra@angular.dev>
2026-07-30 08:29:18 +0000

    082cac9 build: use genrule for best practices copy

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.0+sha-164d77f');
+exports.VERSION = new Version('22.2.0-next.0+sha-082cac9');
 //# sourceMappingURL=version.js.map
