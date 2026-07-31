16136b88ede304702cada53c6ef4432e7bf6d2bb
Angular Builds <dev-infra@angular.dev>
2026-07-31 21:08:03 +0000

    6af906c fix(@angular/ssr): settle writeResponseToNodeResponse when client disconnects

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.2+sha-f837b78');
+exports.VERSION = new Version('22.1.2+sha-6af906c');
 //# sourceMappingURL=version.js.map
