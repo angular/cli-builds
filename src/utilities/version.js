6b82aee8a82bbe99e3a1c38fb54e3474bc6d9730
Angular Builds <dev-infra@angular.dev>
2026-07-31 18:14:13 +0000

    917d3b2 perf(@angular/ssr): avoid buffering request body when sanitizing headers

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.2+sha-72ae936');
+exports.VERSION = new Version('22.1.2+sha-917d3b2');
 //# sourceMappingURL=version.js.map
