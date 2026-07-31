dace910f2e121a3f8d5f05c15aa5adbf04355886
Angular Builds <dev-infra@angular.dev>
2026-07-31 22:27:35 +0000

    1c2a17e ci: use W3C-compliant Sauce Labs tunnelName and remove deprecated tunnelIdentifier

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.0+sha-b74eef8');
+exports.VERSION = new Version('22.2.0-next.0+sha-1c2a17e');
 //# sourceMappingURL=version.js.map
