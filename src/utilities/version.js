83f27c6712f31a2ec385f23a1cd5dee1b2804bb5
Angular Builds <dev-infra@angular.dev>
2026-08-01 08:02:40 +0000

    c645967 ci: fix Sauce Labs E2E capabilities and SSL bumping in Sauce Connect 5

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.2+sha-3396be8');
+exports.VERSION = new Version('22.1.2+sha-c645967');
 //# sourceMappingURL=version.js.map
