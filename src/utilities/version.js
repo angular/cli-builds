1b08b7fbe9a13dc507d5dee160dc374932e29abe
Angular Builds <dev-infra@angular.dev>
2026-08-01 07:32:43 +0000

    3396be8 ci: use proxyLocalhost action input in Sauce Connect CI workflow

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.2+sha-10ba6d7');
+exports.VERSION = new Version('22.1.2+sha-3396be8');
 //# sourceMappingURL=version.js.map
