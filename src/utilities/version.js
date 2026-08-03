4cef65faef69a8755c73260567c23edec39364a9
Angular Builds <dev-infra@angular.dev>
2026-08-03 10:21:12 +0000

    d520dca refactor(@angular/build): implement generic persistent load result cache infrastructure for build pipeline

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.0+sha-3039776');
+exports.VERSION = new Version('22.2.0-next.0+sha-d520dca');
 //# sourceMappingURL=version.js.map
