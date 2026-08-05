61816dc605b769e4e757506963ad8c7380543e55
Angular Builds <dev-infra@angular.dev>
2026-08-05 15:37:41 +0000

    689b5b1 fix(@angular/build): set target for Rolldown dependency prebundling in Vite dev server

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.3+sha-60078fc');
+exports.VERSION = new Version('22.1.3+sha-689b5b1');
 //# sourceMappingURL=version.js.map
