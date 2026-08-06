6f2500bd2b08902fc48e66b5f6f6c9c61e6cd277
Angular Builds <dev-infra@angular.dev>
2026-08-06 23:19:29 +0000

    5ee5420 refactor(@angular/build): bypass worker dispatch for untransformed files in JS transformer

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.2+sha-715852b');
+exports.VERSION = new Version('22.2.0-next.2+sha-5ee5420');
 //# sourceMappingURL=version.js.map
