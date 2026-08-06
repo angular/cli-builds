ec00a97c81208fadc02606ddc502bac9a58195da
Angular Builds <dev-infra@angular.dev>
2026-08-06 08:04:28 +0000

    9a9e745 build: update dependency bazel_lib to v3.7.1

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.3+sha-67a2989');
+exports.VERSION = new Version('22.1.3+sha-9a9e745');
 //# sourceMappingURL=version.js.map
