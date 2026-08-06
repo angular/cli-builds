f6aea74cf4eb0739a4ff3f3d1da237d89a114898
Angular Builds <dev-infra@angular.dev>
2026-08-06 14:52:58 +0000

    199a864 fix(@angular/build): prevent syntax corruption for Crockford-style enum IIFE

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.2+sha-04b5327');
+exports.VERSION = new Version('22.2.0-next.2+sha-199a864');
 //# sourceMappingURL=version.js.map
