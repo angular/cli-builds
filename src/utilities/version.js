7ad8a3f962442f37a41e30b0ae74a829016f32cf
Angular Builds <dev-infra@angular.dev>
2026-08-06 15:02:35 +0000

    c86e932 build: update dependency @modelcontextprotocol/sdk to v1.30.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.3+sha-c805e5c');
+exports.VERSION = new Version('22.1.3+sha-c86e932');
 //# sourceMappingURL=version.js.map
