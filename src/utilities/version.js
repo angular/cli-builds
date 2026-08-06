1404047d870eb14f7b15666008cecb8f0ee728b6
Angular Builds <dev-infra@angular.dev>
2026-08-06 12:13:33 +0000

    04b5327 fix(@angular/build): count statically imported chunks in the initial total

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.2+sha-368535a');
+exports.VERSION = new Version('22.2.0-next.2+sha-04b5327');
 //# sourceMappingURL=version.js.map
