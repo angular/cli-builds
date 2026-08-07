20388d2b9898749bccd546e3a5ca82785f068731
Angular Builds <dev-infra@angular.dev>
2026-08-07 19:14:20 +0000

    0d98516 fix(@angular/build): recursively ignore output and cache paths in watch mode

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.2+sha-d0898e1');
+exports.VERSION = new Version('22.2.0-next.2+sha-0d98516');
 //# sourceMappingURL=version.js.map
