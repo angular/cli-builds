f3e7f08187713d157d329444ae1c6c250958eba0
Angular Builds <dev-infra@angular.dev>
2026-08-07 13:35:34 +0000

    3c7ac15 fix(@angular/build): return only lowest version per target engine

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.2+sha-cda8830');
+exports.VERSION = new Version('22.2.0-next.2+sha-3c7ac15');
 //# sourceMappingURL=version.js.map
