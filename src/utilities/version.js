b1b3dc92eb1554b812aa9628a80c1c3ce3a12ade
Angular Builds <dev-infra@angular.dev>
2026-08-07 17:36:24 +0000

    69bf55c test(@angular/build): ensure hash init in persistent load cache tests

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.2+sha-a6ef9cf');
+exports.VERSION = new Version('22.2.0-next.2+sha-69bf55c');
 //# sourceMappingURL=version.js.map
