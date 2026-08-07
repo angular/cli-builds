c0ad1434f580dbb3852bdbdabf6d62d745144c47
Angular Builds <dev-infra@angular.dev>
2026-08-07 18:42:35 +0000

    bd8a3cb test: optimize and stabilize e2e tests

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.3+sha-9440432');
+exports.VERSION = new Version('22.1.3+sha-bd8a3cb');
 //# sourceMappingURL=version.js.map
