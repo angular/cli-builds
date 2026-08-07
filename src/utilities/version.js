306c793a7f47e2303f6d5f5de1a25dace378d8cb
Angular Builds <dev-infra@angular.dev>
2026-08-07 18:24:35 +0000

    9440432 perf(@angular/cli): avoid eager module loading during global bootstrap

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.3+sha-78f8c5f');
+exports.VERSION = new Version('22.1.3+sha-9440432');
 //# sourceMappingURL=version.js.map
