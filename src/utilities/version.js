d8ed7d1c2ff5b7a7bebc38873d0dded9e5cafc4a
Angular Builds <dev-infra@angular.dev>
2026-08-10 16:03:53 +0000

    c536ae3 fix(@schematics/angular): import UrlSegment instead of subPath in guard generator

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.2+sha-1161e6c');
+exports.VERSION = new Version('22.2.0-next.2+sha-c536ae3');
 //# sourceMappingURL=version.js.map
