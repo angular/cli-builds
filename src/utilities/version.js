ee2a907554881a6f5e22e97d7fb855297ef9a04b
Angular Builds <dev-infra@angular.dev>
2026-08-11 22:28:43 +0000

    9020ddd build: update dependency yargs to v18.1.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.3+sha-898ce69');
+exports.VERSION = new Version('22.1.3+sha-9020ddd');
 //# sourceMappingURL=version.js.map
