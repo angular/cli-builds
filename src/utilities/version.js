39b650e6351d8b041e7d83674a5f9946e15f3759
Angular Builds <dev-infra@angular.dev>
2026-08-11 14:54:39 +0000

    796b576 fix(@angular/build): normalize setupFiles paths to POSIX for vitest runner

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.3+sha-2df0e9f');
+exports.VERSION = new Version('22.1.3+sha-796b576');
 //# sourceMappingURL=version.js.map
