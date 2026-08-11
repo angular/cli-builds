2c12a5c4be9bebe5c4a80084140664d2691dd5fa
Angular Builds <dev-infra@angular.dev>
2026-08-11 06:58:04 +0000

    2df0e9f build: update esbuild dependencies to v0.28.2

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.3+sha-587accb');
+exports.VERSION = new Version('22.1.3+sha-2df0e9f');
 //# sourceMappingURL=version.js.map
