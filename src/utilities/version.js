e02952468b93ffb5a5cc59c38946645a9f5a5422
Angular Builds <dev-infra@angular.dev>
2026-08-11 16:58:09 +0000

    898ce69 ci: limit schematics dependencies rule to minor and patch updates

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.3+sha-796b576');
+exports.VERSION = new Version('22.1.3+sha-898ce69');
 //# sourceMappingURL=version.js.map
