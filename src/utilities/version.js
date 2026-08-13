b0078bfba5c9fa9da0fccb6c59e0b22110535ce6
Angular Builds <dev-infra@angular.dev>
2026-08-13 14:23:29 +0000

    a644998 refactor(@angular/build): lazy load beasties in critical css processor

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.2+sha-ec5aecc');
+exports.VERSION = new Version('22.2.0-next.2+sha-a644998');
 //# sourceMappingURL=version.js.map
