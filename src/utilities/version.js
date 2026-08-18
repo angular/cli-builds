5979e24c3f8e9663f007364d9f1fc7056ac5c212
Angular Builds <dev-infra@angular.dev>
2026-08-18 20:43:00 +0000

    93d9cff refactor(@angular-devkit/schematics): add missing comma to reordered component properties

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.4+sha-0e36040');
+exports.VERSION = new Version('22.1.4+sha-93d9cff');
 //# sourceMappingURL=version.js.map
