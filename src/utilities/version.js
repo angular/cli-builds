e03227ff6667932784e37940a43286564f4fe6a9
Angular Builds <dev-infra@angular.dev>
2026-08-19 16:52:51 +0000

    262562d docs: mention Vitest instead of Karma in README template of schematic library files

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.4+sha-712e74e');
+exports.VERSION = new Version('22.1.4+sha-262562d');
 //# sourceMappingURL=version.js.map
