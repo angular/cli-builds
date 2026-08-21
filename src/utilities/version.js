2716360b0bdfd0f6a31f20571af832e36b0768f7
Angular Builds <dev-infra@angular.dev>
2026-08-21 17:38:48 +0000

    a9e507a build: remove scorecard workflow

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.4+sha-4347639');
+exports.VERSION = new Version('22.2.0-next.4+sha-a9e507a');
 //# sourceMappingURL=version.js.map
