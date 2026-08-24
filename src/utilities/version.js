15c780fea2daf3a43729195689ad965217b3d3b0
Angular Builds <dev-infra@angular.dev>
2026-08-24 17:23:48 +0000

    6acadf2 refactor(@angular/build): parallelize multi-locale inlining with 2D task sharding

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.4+sha-204eafc');
+exports.VERSION = new Version('22.2.0-next.4+sha-6acadf2');
 //# sourceMappingURL=version.js.map
