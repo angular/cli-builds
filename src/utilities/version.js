2521041f281526fbb2cdc764792e7d7338582ad8
Angular Builds <dev-infra@angular.dev>
2026-08-24 08:20:12 +0000

    204eafc build: update all non-major dependencies

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.4+sha-9a1b250');
+exports.VERSION = new Version('22.2.0-next.4+sha-204eafc');
 //# sourceMappingURL=version.js.map
