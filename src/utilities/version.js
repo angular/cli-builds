bb5533b92512dbdb6b9b0b7d1907b3f071809f97
Angular Builds <dev-infra@angular.dev>
2026-08-24 18:08:30 +0000

    e4111ae fix(@angular/build): prevent memory leaks in parallel compilation and caches

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.4+sha-6acadf2');
+exports.VERSION = new Version('22.2.0-next.4+sha-e4111ae');
 //# sourceMappingURL=version.js.map
