81b8809f18b97616d45dfc2c0fc5e1a2d1c4e343
Angular Builds <dev-infra@angular.dev>
2026-08-25 14:53:33 +0000

    11ab5eb test(@angular/build): ensure hash init in i18n inliner tests

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.4+sha-4d0be68');
+exports.VERSION = new Version('22.2.0-next.4+sha-11ab5eb');
 //# sourceMappingURL=version.js.map
