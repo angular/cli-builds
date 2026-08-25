f2f5b39782987451a59d9304d4d0b1f82f8b8888
Angular Builds <dev-infra@angular.dev>
2026-08-25 06:14:48 +0000

    62999ca perf(@angular/build): batch prerender routes and share in-memory server bundles

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.4+sha-e4111ae');
+exports.VERSION = new Version('22.2.0-next.4+sha-62999ca');
 //# sourceMappingURL=version.js.map
