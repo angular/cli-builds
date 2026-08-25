2cc0ef60492e62c49897ee6cccea7fdcbcd8379f
Angular Builds <dev-infra@angular.dev>
2026-08-25 18:22:55 +0000

    a877aa7 perf(@angular/build): implement SharedArrayBuffer translation dictionaries for zero-copy worker access

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.4+sha-11ab5eb');
+exports.VERSION = new Version('22.2.0-next.4+sha-a877aa7');
 //# sourceMappingURL=version.js.map
