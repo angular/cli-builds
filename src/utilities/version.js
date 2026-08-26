ffb797762d67faad684d1da451f2cf284c8a94ee
Angular Builds <dev-infra@angular.dev>
2026-08-26 08:01:33 +0000

    07f9122 release: cut the v22.2.0-next.5 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.4+sha-cf4cd2e');
+exports.VERSION = new Version('22.2.0-next.5+sha-07f9122');
 //# sourceMappingURL=version.js.map
