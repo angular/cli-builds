a88884356ac73bf981f8fd469d151705facfc3b9
Angular Builds <dev-infra@angular.dev>
2026-08-26 07:13:50 +0000

    745c973 build: update pnpm to v11.24.0

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.5+sha-3f132ea');
+exports.VERSION = new Version('22.1.5+sha-745c973');
 //# sourceMappingURL=version.js.map
