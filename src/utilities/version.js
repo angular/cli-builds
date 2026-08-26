fbecee52040f6131ca6d7e55eab56fc5a6ffa4a4
Angular Builds <dev-infra@angular.dev>
2026-08-26 17:43:59 +0000

    ecbcd87 perf(@angular/build): use native sass-embedded compiler daemon

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.5+sha-07f9122');
+exports.VERSION = new Version('22.2.0-next.5+sha-ecbcd87');
 //# sourceMappingURL=version.js.map
