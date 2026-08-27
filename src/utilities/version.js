b5435c2524bdd09d55a847fb3bdd439164fe0485
Angular Builds <dev-infra@angular.dev>
2026-08-27 15:36:43 +0000

    7634b27 refactor(@angular/build): optimize unmodified file and locale handling in i18n inliner

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.5+sha-bbaa07b');
+exports.VERSION = new Version('22.2.0-next.5+sha-7634b27');
 //# sourceMappingURL=version.js.map
