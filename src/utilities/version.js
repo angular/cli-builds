df3b5d79cc645700e982ca8c92e6778b99202e83
Angular Builds <dev-infra@angular.dev>
2026-08-28 15:52:28 +0000

    645e41a fix(@angular/ssr): ensure public directory containment in CommonEngine

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('21.2.22+sha-d3aaa48');
+exports.VERSION = new Version('21.2.22+sha-645e41a');
 //# sourceMappingURL=version.js.map
