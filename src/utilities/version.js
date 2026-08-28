0a4fdcc91a10f2f35a6da16cf2dd7c520344e589
Angular Builds <dev-infra@angular.dev>
2026-08-28 15:27:53 +0000

    4b8c62a fix(@angular/build): add splitting option to unit-test builder

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.5+sha-2b85529');
+exports.VERSION = new Version('22.2.0-next.5+sha-4b8c62a');
 //# sourceMappingURL=version.js.map
