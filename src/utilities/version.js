79ce84ca0cf5d174169ddfbc0b24bcda579d70a5
Angular Builds <dev-infra@angular.dev>
2026-08-31 13:19:48 +0000

    f1fd823 fix(@angular/build): fail build and exclude routes when prerendering fails

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.6+sha-70748ca');
+exports.VERSION = new Version('22.1.6+sha-f1fd823');
 //# sourceMappingURL=version.js.map
