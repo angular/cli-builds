f12a4fcb197c2f0c098ee076c8577d35e218d013
Angular Builds <dev-infra@angular.dev>
2026-09-01 13:48:35 +0000

    f329174 refactor(@angular/build): eliminate NoopCompilation and introduce primary and secondary compilation contexts

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.5+sha-2dc9aae');
+exports.VERSION = new Version('22.2.0-next.5+sha-f329174');
 //# sourceMappingURL=version.js.map
