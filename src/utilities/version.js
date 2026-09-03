468c15ba3591d60ccae968dfb51ef67e20fe7043
Angular Builds <dev-infra@angular.dev>
2026-09-03 16:32:19 +0000

    8eed511 test: allow git dependencies in E2E npm sandbox configuration

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-54d712f');
+exports.VERSION = new Version('22.2.0-next.6+sha-8eed511');
 //# sourceMappingURL=version.js.map
