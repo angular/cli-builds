71fbe5bb21de58caf55f81281d03faccbd862cc6
Angular Builds <dev-infra@angular.dev>
2026-09-03 13:18:23 +0000

    14c54b3 refactor(@angular/build): unify configuration loading and option transformations in TypeScriptCompilation

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-675f1f5');
+exports.VERSION = new Version('22.2.0-next.6+sha-14c54b3');
 //# sourceMappingURL=version.js.map
