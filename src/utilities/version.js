91c2b95b1aa170ac1b2f65406d911394d985976a
Angular Builds <dev-infra@angular.dev>
2026-09-03 15:01:49 +0000

    55583c4 build: update beasties to 0.5.1

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-f6cb128');
+exports.VERSION = new Version('22.2.0-next.6+sha-55583c4');
 //# sourceMappingURL=version.js.map
