622edb4049b91b3db31837aa6b964dafbd86ae01
Angular Builds <dev-infra@angular.dev>
2026-09-03 14:43:49 +0000

    f6cb128 test: update e2e global npm version to 12.0.2

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-3b01482');
+exports.VERSION = new Version('22.2.0-next.6+sha-f6cb128');
 //# sourceMappingURL=version.js.map
