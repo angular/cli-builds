5857896ccf1ff421bcf887663e3156d53a61331e
Angular Builds <dev-infra@angular.dev>
2026-09-07 15:24:20 +0000

    6b20983 build: update lockfiles for dependencies

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-26a59de');
+exports.VERSION = new Version('22.2.0-next.6+sha-6b20983');
 //# sourceMappingURL=version.js.map
