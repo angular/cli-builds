529c462ab1053229c3153a8f7f0c0ef7bd1e4a32
Angular Builds <dev-infra@angular.dev>
2026-09-08 15:21:38 +0000

    d396324 fix(@angular/build): default preloadInitial to false when serviceWorker is enabled

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-6b20983');
+exports.VERSION = new Version('22.2.0-next.6+sha-d396324');
 //# sourceMappingURL=version.js.map
