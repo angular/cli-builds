dbd5cd16c102bc140030034718f6116185ffa67a
Angular Builds <dev-infra@angular.dev>
2026-09-08 16:56:35 +0000

    d11a663 perf(@angular/build): verify cache metadata before reading file and eliminate sqlite read-locks

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-f1afa60');
+exports.VERSION = new Version('22.2.0-next.6+sha-d11a663');
 //# sourceMappingURL=version.js.map
