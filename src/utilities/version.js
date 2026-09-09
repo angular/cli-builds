72438b93c7bb11c625a45186847cee1987d7ee76
Angular Builds <dev-infra@angular.dev>
2026-09-09 14:31:38 +0000

    87a1610 test(@angular/build): fix flakiness in dev-server rebuild error detection test

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.1.7+sha-7e133c4');
+exports.VERSION = new Version('22.1.7+sha-87a1610');
 //# sourceMappingURL=version.js.map
