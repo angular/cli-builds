175b8077f6586e9555c397f480c4f630bd927e31
Angular Builds <dev-infra@angular.dev>
2026-09-09 13:57:20 +0000

    0ba4d8e perf(@angular/build): reduce watcher debounce latency for faster incremental rebuilds

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-062ac27');
+exports.VERSION = new Version('22.2.0-next.6+sha-0ba4d8e');
 //# sourceMappingURL=version.js.map
