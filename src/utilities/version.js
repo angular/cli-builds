6923f3bde86bd58e073e13a673ef7ce5d95f1159
Angular Builds <dev-infra@angular.dev>
2026-09-09 15:18:04 +0000

    482f6d8 perf(@angular/build): decouple diagnostic type checking from build start

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-ebdf495');
+exports.VERSION = new Version('22.2.0-next.6+sha-482f6d8');
 //# sourceMappingURL=version.js.map
