49bff5943fb234f99a3d9414af3f5ceda4fa8660
Angular Builds <dev-infra@angular.dev>
2026-09-10 14:24:27 +0000

    d213df7 test(@angular/build): prevent flakiness in incremental-watch e2e test

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-17d2187');
+exports.VERSION = new Version('22.2.0-next.6+sha-d213df7');
 //# sourceMappingURL=version.js.map
