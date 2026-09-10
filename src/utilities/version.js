685873ed6ee3ca224284e3e41745fa3f91de5597
Angular Builds <dev-infra@angular.dev>
2026-09-10 14:08:21 +0000

    17d2187 build: update dependency rolldown to v1.2.8

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-next.6+sha-44129f6');
+exports.VERSION = new Version('22.2.0-next.6+sha-17d2187');
 //# sourceMappingURL=version.js.map
