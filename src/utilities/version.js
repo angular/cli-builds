0b4e1fb5988720556fd649bcd70cb5f6bbf9b705
Angular Builds <dev-infra@angular.dev>
2026-09-17 16:42:25 +0000

    93c118e refactor(@angular/build): cap default i18n inlining concurrency to 8

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.3.0-next.0+sha-8235b43');
+exports.VERSION = new Version('22.3.0-next.0+sha-93c118e');
 //# sourceMappingURL=version.js.map
