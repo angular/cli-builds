097ded927dc71fc4ae74b1460771e7013580faf0
Angular Builds <dev-infra@angular.dev>
2026-09-17 23:12:57 +0000

    96832f1 refactor(@angular/build): extract shared concurrency, watcher, and styling utilities

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-cf21837');
+exports.VERSION = new Version('22.2.0-rc.0+sha-96832f1');
 //# sourceMappingURL=version.js.map
