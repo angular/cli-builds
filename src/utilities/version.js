098d4bae79f7c8a4373e66336da4bf8408f829fc
Angular Builds <dev-infra@angular.dev>
2026-09-17 18:32:12 +0000

    cf21837 build: update cross-repo angular dependencies

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-9c6037a');
+exports.VERSION = new Version('22.2.0-rc.0+sha-cf21837');
 //# sourceMappingURL=version.js.map
