2c55ffde4b56b50a1ac949205f39221863411879
Angular Builds <dev-infra@angular.dev>
2026-09-17 18:09:55 +0000

    2b59d84 refactor: add package.json exports to architect and schematics

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.3.0-next.0+sha-93c118e');
+exports.VERSION = new Version('22.3.0-next.0+sha-2b59d84');
 //# sourceMappingURL=version.js.map
