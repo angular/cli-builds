c094389fda87390a578453f3196d4275ef10036b
Angular Builds <dev-infra@angular.dev>
2026-09-18 14:10:16 +0000

    4ebaa66 fix(@schematics/angular): update @types/node to a version vitest 5 accepts

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-c0e6b3c');
+exports.VERSION = new Version('22.2.0-rc.0+sha-4ebaa66');
 //# sourceMappingURL=version.js.map
