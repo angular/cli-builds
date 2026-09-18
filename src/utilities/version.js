fe530b5b9687b75b9b2673c7639f4bf2623aed3b
Angular Builds <dev-infra@angular.dev>
2026-09-18 16:44:49 +0000

    ee30413 fix(@angular/build): ensure chokidar watcher is ready before returning

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-4ebaa66');
+exports.VERSION = new Version('22.2.0-rc.0+sha-ee30413');
 //# sourceMappingURL=version.js.map
