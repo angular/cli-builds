ef3d01887c6211c747e0c4b6bcf2219251ae8a34
Angular Builds <dev-infra@angular.dev>
2026-09-18 09:06:34 +0000

    c0e6b3c fix(@angular-devkit/core): name the unknown option in a schema validation error

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-96832f1');
+exports.VERSION = new Version('22.2.0-rc.0+sha-c0e6b3c');
 //# sourceMappingURL=version.js.map
