e3cf8ad9410f001a83b6e0d50e29703d6750e51d
Angular Builds <dev-infra@angular.dev>
2026-09-21 18:16:11 +0000

    1fc2732 fix(@angular/build): support parenthesized expressions in oxc linker

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-5cd20e1');
+exports.VERSION = new Version('22.2.0-rc.0+sha-1fc2732');
 //# sourceMappingURL=version.js.map
