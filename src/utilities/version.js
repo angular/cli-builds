752e9caa8971acc7e48f23df96a5fbeeeb1972e7
Angular Builds <dev-infra@angular.dev>
2026-09-21 13:45:30 +0000

    e7ebbcd refactor(@angular/build): relocate non-esbuild utilities out of tools/esbuild

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-ee30413');
+exports.VERSION = new Version('22.2.0-rc.0+sha-e7ebbcd');
 //# sourceMappingURL=version.js.map
