405a39cfaf2c308536bd9690dce558f37d7549c8
Angular Builds <dev-infra@angular.dev>
2026-09-22 15:31:18 +0000

    f47f77f fix(@angular/build): only compile included test files in unit-test builder

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-80acd62');
+exports.VERSION = new Version('22.2.0-rc.0+sha-f47f77f');
 //# sourceMappingURL=version.js.map
