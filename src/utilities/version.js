4c4e343b227dd2eacdd3765c904417fce0a3c1ac
Angular Builds <dev-infra@angular.dev>
2026-09-22 15:16:31 +0000

    80acd62 fix(@angular/build): execute setup file hooks for each spec file when coverage is enabled

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-1fc2732');
+exports.VERSION = new Version('22.2.0-rc.0+sha-80acd62');
 //# sourceMappingURL=version.js.map
