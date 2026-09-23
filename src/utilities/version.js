6bd0064eff9e3c0a3a3d33987e532720130a7870
Angular Builds <dev-infra@angular.dev>
2026-09-23 20:09:54 +0000

    82f1972 refactor: bump framework and `ng-packagr` deps for 22.2.0 release

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0-rc.0+sha-10e2be9');
+exports.VERSION = new Version('22.2.0-rc.0+sha-82f1972');
 //# sourceMappingURL=version.js.map
