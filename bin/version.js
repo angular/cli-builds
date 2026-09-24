2b42fec49e52eb2229b1129ca564557ec2ba8685
Angular Builds <dev-infra@angular.dev>
2026-09-24 17:58:55 +0000

    d968259 docs: fix stale Bazel test targets and toolchain versions

--- bin/version.js
+++ bin/version.js
@@ -15,7 +15,7 @@ var SUPPORTED_NODE_VERSIONS = '^22.22.3 || ^24.15.0 || >=26.0.0';
 /**
  * The version of the Angular CLI.
  */
-var VERSION = '22.3.0-next.0+sha-297ef9c';
+var VERSION = '22.3.0-next.0+sha-d968259';
 
 /**
  * The supported Node.js versions.
