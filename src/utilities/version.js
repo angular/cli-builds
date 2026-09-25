80b3a61a5807e0cc944614a511058704fc0cc656
Angular Builds <dev-infra@angular.dev>
2026-09-25 15:15:52 +0000

    5194b41 fix(@angular/build): prevent chokidar from scanning unimported node_modules in watch mode

--- src/utilities/version.js
+++ src/utilities/version.js
@@ -22,5 +22,5 @@ class Version {
         this.patch = patch;
     }
 }
-exports.VERSION = new Version('22.2.0+sha-37cf399');
+exports.VERSION = new Version('22.2.0+sha-5194b41');
 //# sourceMappingURL=version.js.map
